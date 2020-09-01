9dbfe7937848291e8f8eae6eb7cebd137f683276
Petr Pivoňka <petrpiv1@gmail.com>
2020-09-01 23:22:47 +0200

    CGameCtnMediaBlockColoringCapturable

---
+++ GBX.NET/Engines/Game/CGameCtnMediaBlockColoringCapturable.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBX.NET.Engines.Game
+{
+    [Node(0x0316C000)]
+    public class CGameCtnMediaBlockColoringCapturable : CGameCtnMediaBlock
+    {
+        public CGameCtnMediaBlockColoringCapturable(ILookbackable lookbackable, uint classID) : base(lookbackable, classID)
+        {
+
+        }
+
+        [Chunk(0x0316C000)]
+        public class Chunk000 : Chunk
+        {
+            public int Version { get; set; }
+            public Key[] Keys { get; set; }
+
+            public Chunk000(Node node) : base(node)
+            {
+
+            }
+
+            public override void ReadWrite(GameBoxReaderWriter rw)
+            {
+                Version = rw.Int32(Version);
+                rw.Int32(Unknown);
+
+                Keys = rw.Array(Keys, i => new Key()
+                {
+                    Time = rw.Reader.ReadSingle(),
+                    Hue = rw.Reader.ReadSingle(),
+                    Gauge = rw.Reader.ReadSingle(),
+                    Emblem = rw.Reader.ReadInt32()
+                },
+                x =>
+                {
+                    rw.Writer.Write(x.Time);
+                    rw.Writer.Write(x.Hue);
+                    rw.Writer.Write(x.Gauge);
+                    rw.Writer.Write(x.Emblem);
+                });
+            }
+        }
+
+        public class Key
+        {
+            public float Time { get; set; }
+            public float Hue { get; set; }
+            public float Gauge { get; set; }
+            public int Emblem { get; set; }
+        }
+    }
+}
