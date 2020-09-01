7930646da30d0c73bcae42883e2cc7f9e3531073
Petr Pivoňka <petrpiv1@gmail.com>
2020-09-01 19:43:04 +0200

    CGameCtnMediaBlockBloomHdr

---
+++ GBX.NET/Engines/Game/CGameCtnMediaBlockBloomHdr.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBX.NET.Engines.Game
+{
+    [Node(0x03128000)]
+    public class CGameCtnMediaBlockBloomHdr : CGameCtnMediaBlock
+    {
+        public CGameCtnMediaBlockBloomHdr(ILookbackable lookbackable, uint classID) : base(lookbackable, classID)
+        {
+
+        }
+
+        [Chunk(0x03128002)]
+        public class Chunk002 : Chunk
+        {
+            public Key[] Keys { get; set; }
+
+            public Chunk002(Node node) : base(node)
+            {
+
+            }
+
+            public override void ReadWrite(GameBoxReaderWriter rw)
+            {
+                Keys = rw.Array(Keys, i => new Key()
+                {
+                    Time = rw.Reader.ReadSingle(),
+                    Intensity = rw.Reader.ReadSingle(),
+                    StreaksIntensity = rw.Reader.ReadSingle(),
+                    StreaksAttenuation = rw.Reader.ReadSingle()
+                },
+                x =>
+                {
+                    rw.Writer.Write(x.Time);
+                    rw.Writer.Write(x.Intensity);
+                    rw.Writer.Write(x.StreaksIntensity);
+                    rw.Writer.Write(x.StreaksAttenuation);
+                });
+            }
+        }
+
+        public class Key
+        {
+            public float Time { get; set; }
+            public float Intensity { get; set; }
+            public float StreaksIntensity { get; set; }
+            public float StreaksAttenuation { get; set; }
+        }
+    }
+}
