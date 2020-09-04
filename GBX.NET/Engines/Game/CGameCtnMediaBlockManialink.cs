97ca4d9d19044342fad11c3b7d73a9869192e90b
Petr Pivoňka <petrpiv1@gmail.com>
2020-09-04 19:16:43 +0200

    CGameCtnMediaBlockManialink

---
+++ GBX.NET/Engines/Game/CGameCtnMediaBlockManialink.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBX.NET.Engines.Game
+{
+    [Node(0x0312A000)]
+    public class CGameCtnMediaBlockManialink : CGameCtnMediaBlock
+    {
+        public CGameCtnMediaBlockManialink(ILookbackable lookbackable, uint classID) : base(lookbackable, classID)
+        {
+
+        }
+
+        [Chunk(0x0312A001)]
+        public class Chunk001 : Chunk
+        {
+            public int Version { get; set; }
+            public float Start { get; set; }
+            public float End { get; set; }
+            public string Manialink { get; set; }
+
+            public Chunk001(Node node) : base(node)
+            {
+
+            }
+
+            public override void ReadWrite(GameBoxReaderWriter rw)
+            {
+                Version = rw.Int32(Version);
+                Start = rw.Single(Start);
+                End = rw.Single(End);
+                Manialink = rw.String(Manialink);
+            }
+        }
+    }
+}
