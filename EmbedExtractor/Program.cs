cae14a81d0e83ca3233a9c9923b8014884543d57
Petr Pivoňka <petrpiv1@gmail.com>
2020-09-20 02:29:13 +0200

    EmbedExtractor

---
+++ EmbedExtractor/Program.cs
@@ -0,0 +1,33 @@
+﻿
+using GBX.NET;
+using GBX.NET.Engines.Game;
+using ICSharpCode.SharpZipLib.Zip;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace EmbedExtractor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = args.FirstOrDefault();
+            if (fileName == null) return;
+
+            Log.OnLogEvent += Log_OnLogEvent;
+
+            var gbx = GameBox.Parse(fileName);
+
+            if (gbx is GameBox<CGameCtnChallenge> gbxMap)
+                gbxMap.MainNode.ExportEmbedZip(gbxMap.FileName + ".zip");
+        }
+
+        private static void Log_OnLogEvent(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
