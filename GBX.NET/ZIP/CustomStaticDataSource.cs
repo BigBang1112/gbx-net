cf80a7771f2fca32842665df9b05db0651505ef1
Petr Pivoňka <petrpiv1@gmail.com>
2020-11-11 16:42:45 +0100

    Include CustomStaticDataSource

---
+++ GBX.NET/ZIP/CustomStaticDataSource.cs
@@ -0,0 +1,17 @@
+﻿using ICSharpCode.SharpZipLib.Zip;
+using System.IO;
+
+namespace GBX.NET.ZIP
+{
+    public class CustomStaticDataSource : IStaticDataSource
+    {
+        private Stream stream;
+        public Stream GetSource() => stream;
+
+        public void SetStream(Stream inputStream)
+        {
+            stream = inputStream;
+            stream.Position = 0;
+        }
+    }
+}
