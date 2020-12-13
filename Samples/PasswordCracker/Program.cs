ec7750b9f1f5c2a157e162f51b2659a86a584956
Petr Pivoňka <petrpiv1@gmail.com>
2020-12-13 03:13:41 +0100

    Add password cracker sample project

---
+++ Samples/PasswordCracker/Program.cs
@@ -0,0 +1,34 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using System;
+using System.IO;
+using System.Linq;
+
+namespace PasswordCracker
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var fileName = args.FirstOrDefault();
+            if (fileName == null) return;
+
+            Log.OnLogEvent += Log_OnLogEvent;
+
+            var gbx = GameBox.Parse(fileName);
+
+            if (gbx is GameBox<CGameCtnChallenge> gbxMap)
+            {
+                gbxMap.MainNode.CrackPassword();
+                gbxMap.Save(Path.GetFileName(fileName));
+            }
+        }
+
+        private static void Log_OnLogEvent(string text, ConsoleColor color)
+        {
+            Console.ForegroundColor = color;
+            Console.WriteLine(text);
+            Console.ResetColor();
+        }
+    }
+}
