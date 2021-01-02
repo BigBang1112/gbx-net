5a5dce859a73c3fc8616b4cc267665bb11c4ccaa
Petr Pivoňka <petrpiv1@gmail.com>
2021-01-02 23:16:18 +0100

    Fix CGameCtnMediaBlockFog

--- GBX.NET/Engines/Game/CGameCtnMediaBlockFog.cs
+++ GBX.NET/Engines/Game/CGameCtnMediaBlockFog.cs
@@ -26,24 +26,69 @@ namespace GBX.NET.Engines.Game
         {
             public int Version { get; set; }
 
-            public override void ReadWrite(CGameCtnMediaBlockFog n, GameBoxReaderWriter rw)
+            public override void Read(CGameCtnMediaBlockFog n, GameBoxReader r, GameBoxWriter unknownW)
             {
-                Version = rw.Int32(Version);
-                n.Keys = rw.Array(n.Keys, i =>
+                Version = r.ReadInt32();
+                n.Keys = r.ReadArray(i =>
                 {
-                    var time = rw.Reader.ReadSingle();
-                    var unknown = rw.Reader.ReadArray<float>(9);
+                    var time = r.ReadSingle();
+                    var intensity = r.ReadSingle();
+                    var skyIntensity = r.ReadSingle();
+                    var distance = r.ReadSingle();
+
+                    float? coefficient = null;
+                    Vec3? color = null;
+                    float? cloudsOpacity = null;
+                    float? cloudsSpeed = null;
+
+                    if (Version >= 1)
+                    {
+                        coefficient = r.ReadSingle();
+                        color = r.ReadVec3();
+
+                        if (Version >= 2)
+                        {
+                            cloudsOpacity = r.ReadSingle();
+                            cloudsSpeed = r.ReadSingle();
+                        }
+                    }
 
                     return new Key()
                     {
                         Time = time,
-                        Unknown = unknown
+                        Intensity = intensity,
+                        SkyIntensity = skyIntensity,
+                        Distance = distance,
+                        Coefficient = coefficient,
+                        Color = color,
+                        CloudsOpacity = cloudsOpacity,
+                        CloudsSpeed = cloudsSpeed
                     };
-                },
-                x =>
+                });
+            }
+
+            public override void Write(CGameCtnMediaBlockFog n, GameBoxWriter w, GameBoxReader unknownR)
+            {
+                w.Write(Version);
+
+                w.Write(n.Keys, x =>
                 {
-                    rw.Writer.Write(x.Time);
-                    rw.Writer.Write(x.Unknown);
+                    w.Write(x.Time);
+                    w.Write(x.Intensity);
+                    w.Write(x.SkyIntensity);
+                    w.Write(x.Distance);
+
+                    if (Version >= 1)
+                    {
+                        w.Write(x.Coefficient.GetValueOrDefault(1));
+                        w.Write(x.Coefficient.GetValueOrDefault());
+
+                        if (Version >= 2)
+                        {
+                            w.Write(x.CloudsOpacity.GetValueOrDefault(1));
+                            w.Write(x.CloudsSpeed.GetValueOrDefault(1));
+                        }
+                    }
                 });
             }
         }
@@ -56,7 +101,13 @@ namespace GBX.NET.Engines.Game
 
         public class Key : MediaBlockKey
         {
-            public float[] Unknown { get; set; }
+            public float Intensity { get; set; }
+            public float SkyIntensity { get; set; }
+            public float Distance { get; set; }
+            public float? Coefficient { get; set; }
+            public Vec3? Color { get; set; }
+            public float? CloudsOpacity { get; set; }
+            public float? CloudsSpeed { get; set; }
         }
 
         #endregion
