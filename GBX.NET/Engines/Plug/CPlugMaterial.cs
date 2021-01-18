af9233a3f548a95f68dc889e7407a9e75faf443d
Petr Pivoňka <petrpiv1@gmail.com>
2021-01-19 00:44:13 +0100

    Add CPlugMaterial, CPlugMaterialCustom

---
+++ GBX.NET/Engines/Plug/CPlugMaterial.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBX.NET.Engines.Plug
+{
+    [Node(0x09079000)]
+    public class CPlugMaterial : CPlug
+    {
+        [Chunk(0x09079001)]
+        public class Chunk09079001 : Chunk<CPlugMaterial>
+        {
+            public override void ReadWrite(CPlugMaterial n, GameBoxReaderWriter rw)
+            {
+                rw.Int32(Unknown);
+            }
+        }
+
+        [Chunk(0x09079007)]
+        public class Chunk09079007 : Chunk<CPlugMaterial>
+        {
+            public override void ReadWrite(CPlugMaterial n, GameBoxReaderWriter rw)
+            {
+                var customMaterial = rw.Reader.ReadNodeRef<CPlugMaterialCustom>();
+            }
+        }
+    }
+}
