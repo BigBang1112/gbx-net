58ec1c9b206c94e6325f24b6830a0ec0b8f903bf
Petr Pivoňka <petrpiv1@gmail.com>
2021-03-02 20:36:14 +0100

    Add CGamePlayerScore

---
+++ GBX.NET/Engines/Game/CGamePlayerScore.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBX.NET.Engines
+{
+    [Node(0x0308D000)]
+    public class CGamePlayerScore : Node
+    {
+    }
+}
