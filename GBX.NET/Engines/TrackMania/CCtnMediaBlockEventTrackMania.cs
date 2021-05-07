f89f4d3245a1ab9af1e87516b2c9b4776cffcc8d
Petr Pivoňka <petrpiv1@gmail.com>
2021-05-07 17:07:10 +0200

    Change Time to TimeSpan

--- GBX.NET/Engines/TrackMania/CCtnMediaBlockEventTrackMania.cs
+++ GBX.NET/Engines/TrackMania/CCtnMediaBlockEventTrackMania.cs
@@ -83,33 +83,33 @@ namespace GBX.NET.Engines.TrackMania
                 n.End = rw.Single(n.End);
                 U01 = rw.Int32(U01);
 
-                n.Stunts = rw.Array(n.Stunts, i => new Stunt()
+                n.Stunts = rw.Array(n.Stunts, (i, r) => new Stunt()
                 {
-                    Time = rw.Reader.ReadSingle(),
-                    Figure = (EStuntFigure)rw.Reader.ReadInt32(),
-                    Angle = rw.Reader.ReadInt32(),
-                    Score = rw.Reader.ReadInt32(),
-                    Factor = rw.Reader.ReadSingle(),
-                    Straight = rw.Reader.ReadBoolean(),
-                    U02 = rw.Reader.ReadInt32(),
-                    U03 = rw.Reader.ReadInt32(),
-                    U04 = rw.Reader.ReadInt32(),
-                    Combo = rw.Reader.ReadInt32(),
-                    TotalScore = rw.Reader.ReadInt32()
+                    Time = TimeSpan.FromSeconds(r.ReadSingle()),
+                    Figure = (EStuntFigure)r.ReadInt32(),
+                    Angle = r.ReadInt32(),
+                    Score = r.ReadInt32(),
+                    Factor = r.ReadSingle(),
+                    Straight = r.ReadBoolean(),
+                    U02 = r.ReadInt32(),
+                    U03 = r.ReadInt32(),
+                    U04 = r.ReadInt32(),
+                    Combo = r.ReadInt32(),
+                    TotalScore = r.ReadInt32()
                 },
-                x =>
+                (x, w) =>
                 {
-                    rw.Writer.Write(x.Time);
-                    rw.Writer.Write((int)x.Figure);
-                    rw.Writer.Write(x.Angle);
-                    rw.Writer.Write(x.Score);
-                    rw.Writer.Write(x.Factor);
-                    rw.Writer.Write(x.Straight);
-                    rw.Writer.Write(x.U02);
-                    rw.Writer.Write(x.U03);
-                    rw.Writer.Write(x.U04);
-                    rw.Writer.Write(x.Combo);
-                    rw.Writer.Write(x.TotalScore);
+                    w.Write((float)x.Time.TotalSeconds);
+                    w.Write((int)x.Figure);
+                    w.Write(x.Angle);
+                    w.Write(x.Score);
+                    w.Write(x.Factor);
+                    w.Write(x.Straight);
+                    w.Write(x.U02);
+                    w.Write(x.U03);
+                    w.Write(x.U04);
+                    w.Write(x.Combo);
+                    w.Write(x.TotalScore);
                 });
             }
         }
@@ -135,7 +135,7 @@ namespace GBX.NET.Engines.TrackMania
 
         public class Stunt
         {
-            public float Time { get; set; }
+            public TimeSpan Time { get; set; }
             public EStuntFigure Figure { get; set; }
             public int Angle { get; set; }
             public int Score { get; set; }
