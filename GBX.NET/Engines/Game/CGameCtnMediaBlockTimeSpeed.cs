1d4a66c153c6bcb27ee233926cefd23ccfbc2a45
Petr Pivoňka <petrpiv1@gmail.com>
2021-05-14 21:21:28 +0200

    Add CGameCtnMediaBlockTimeSpeed

---
+++ GBX.NET/Engines/Game/CGameCtnMediaBlockTimeSpeed.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GBX.NET.Engines.Game
+{
+    [Node(0x03129000)]
+    public class CGameCtnMediaBlockTimeSpeed : CGameCtnMediaBlockTime
+    {
+        
+    }
+}
