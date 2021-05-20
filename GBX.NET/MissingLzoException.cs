a81a78c51b517abe0350a79df5f7c5bf4ba93783
Petr Pivoňka <petrpiv1@gmail.com>
2021-05-20 03:29:45 +0200

    Uppercase HEADER DATA

--- GBX.NET/MissingLzoException.cs
+++ GBX.NET/MissingLzoException.cs
@@ -8,7 +8,7 @@ namespace GBX.NET
     {
         public override string Message => "To parse a body of this GBX file, an LZO compression algorithm is required.\n\n" +
             "Please reference the GBX.NET.LZO library inside your project.\n\n" +
-            "Note that the GBX.NET.LZO library is licensed under GNU General Public License 3, therefore you HAVE to use the same license for your project.\n\n" +
-            "However, you DON'T HAVE TO reference the library if you're looking to extract the header data only. See the GameBox.ParseHeader method.";
+            "Note that the GBX.NET.LZO library is licensed under GNU General Public License 3, therefore you HAVE to use the same license for your project after referencing.\n\n" +
+            "However, you DON'T HAVE TO reference the library if you're looking to extract the HEADER DATA only. See the GameBox.ParseHeader method.";
     }
 }
