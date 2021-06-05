3fba525dd9e7c06ff2a8e87b8cbffd71c2d99f4a
Petr Pivoňka <petrpiv1@gmail.com>
2021-06-05 23:58:23 +0200

    Add GBX.NET.IO

---
+++ GBX.NET/IO/GameBoxIO.cs
@@ -0,0 +1,15 @@
+﻿namespace GBX.NET.IO
+{
+    public abstract class GameBoxIO<T> : GameBoxIOCustom<T, GameBox<T>> where T : Node
+    {
+        public GameBoxIO(GameBox gbx) : base(gbx)
+        {
+
+        }
+
+        public GameBoxIO(GameBox<T> gbx) : base(gbx)
+        {
+
+        }
+    }
+}
