f950227b11ca2a6215b3dd26a551a414b84a3ec3
Petr Pivoňka <petrpiv1@gmail.com>
2021-08-17 21:46:37 +0200

    Add ParseSaveMapWithoutExceptions test

---
+++ GBX.NET.Tests/Integration/GameBoxTests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace GBX.NET.Tests.Integration
+{
+    public class GameBoxTests
+    {
+        [Theory(DisplayName = "Parse/Save map without exceptions")]
+        [InlineData("CCP#04 - ODYSSEY.Map.Gbx")]
+        public void ParseSaveMapWithoutExceptions(string fileName)
+        {
+            var node = GameBox.ParseNode(Path.Combine("Files", fileName));
+            node.GBX.Header.CompressionOfBody = GameBoxCompression.Uncompressed;
+            using var ms = new MemoryStream();
+            node.Save(ms);
+        }
+    }
+}
