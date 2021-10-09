2bcdacff98ab536c74cfae39a0cdd2a77213a032
Petr Pivoňka <petrpiv1@gmail.com>
2021-10-09 04:20:06 +0200

    Rename IListExtensions to ListExtensions

--- GBX.NET/Extensions/ListExtensions.cs
+++ GBX.NET/Extensions/ListExtensions.cs
@@ -3,8 +3,16 @@ using System.Collections.Generic;
 
 namespace GBX.NET
 {
-    public static class IListExtensions
+    public static class ListExtensions
     {
+        /// <summary>
+        /// Removes all elements from the list based on a match.
+        /// </summary>
+        /// <typeparam name="T">List type.</typeparam>
+        /// <param name="list">List.</param>
+        /// <param name="match">Match.</param>
+        /// <returns>Amount of removed objects.</returns>
+        /// <exception cref="NotSupportedException">The list is read-only.</exception>
         public static int RemoveAll<T>(this IList<T> list, Predicate<T> match)
         {
             int count = 0;
