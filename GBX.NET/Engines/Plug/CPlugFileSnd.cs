911de3ee0c2c76436295ced365a5f93ab6b98c83
Petr Pivoňka <petrpiv1@gmail.com>
2021-10-10 19:43:01 +0200

    Add CPlugFile

---
+++ GBX.NET/Engines/Plug/CPlugFileSnd.cs
@@ -0,0 +1,11 @@
+﻿namespace GBX.NET.Engines.Plug
+{
+    [Node(0x09030000)]
+    public sealed class CPlugFileSnd : CPlugFile
+    {
+        private CPlugFileSnd()
+        {
+
+        }
+    }
+}
