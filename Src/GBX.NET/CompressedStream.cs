74203a329a79bb5d537e94d70352c91ca3fad657
Petr Pivoňka <petrpiv1@gmail.com>
2021-11-06 05:44:54 +0100

    Make CompressedStream nicer

--- Src/GBX.NET/CompressedStream.cs
+++ Src/GBX.NET/CompressedStream.cs
@@ -14,50 +14,58 @@ namespace GBX.NET
             switch (mode)
             {
                 case CompressionMode.Compress:
-                    stream.WriteByte(0x78);
-
-                    switch (Compression)
-                    {
-                        case CompressionLevel.BestCompression:
-                            stream.WriteByte(0xDA);
-                            break;
-                        case CompressionLevel.DefaultCompression:
-                            stream.WriteByte(0x9C);
-                            break;
-                        case CompressionLevel.NoCompression:
-                            stream.WriteByte(0x01);
-                            break;
-                        case CompressionLevel.UnknownCompression:
-                            throw new Exception("Unknown compression can't be written.");
-                    }
+                    WriteMagic(stream);
                     break;
                 case CompressionMode.Decompress:
-                    var magic = new byte[2];
-                    stream.Read(magic, 0, 2); // Needed for DeflateStream to work
-
-                    if (magic[0] != 0x78)
-                        throw new Exception("Data isn't compressed with Deflate ZLIB");
-
-                    switch (magic[1])
-                    {
-                        case 0x01:
-                            Compression = CompressionLevel.NoCompression;
-                            Debug.WriteLine("Deflate ZLIB - No compression");
-                            break;
-                        case 0x9C:
-                            Compression = CompressionLevel.DefaultCompression;
-                            Debug.WriteLine("Deflate ZLIB - Default compression");
-                            break;
-                        case 0xDA:
-                            Compression = CompressionLevel.BestCompression;
-                            Debug.WriteLine("Deflate ZLIB - Best compression");
-                            break;
-                        default:
-                            Compression = CompressionLevel.UnknownCompression;
-                            Debug.WriteLine("Deflate ZLIB - Unknown compression");
-                            break;
-                    }
+                    Compression = ReadMagic(stream);
                     break;
+            }
+        }
+
+        private CompressionLevel ReadMagic(Stream stream)
+        {
+            var magic = new byte[2];
+            stream.Read(magic, 0, 2); // Needed for DeflateStream to work
+
+            if (magic[0] != 0x78)
+                throw new Exception("Data isn't compressed with Deflate ZLIB");
+
+            var log = magic[1] switch
+            {
+                0x01 => "Deflate ZLIB - No compression",
+                0x9C => "Deflate ZLIB - Default compression",
+                0xDA => "Deflate ZLIB - Best compression",
+                _ => "Deflate ZLIB - Unknown compression",
+            };
+
+            Debug.WriteLine(log);
+
+            return magic[1] switch
+            {
+                0x01 => CompressionLevel.NoCompression,
+                0x9C => CompressionLevel.DefaultCompression,
+                0xDA => CompressionLevel.BestCompression,
+                _ => CompressionLevel.UnknownCompression,
+            };
+        }
+
+        private void WriteMagic(Stream stream)
+        {
+            stream.WriteByte(0x78);
+
+            switch (Compression)
+            {
+                case CompressionLevel.BestCompression:
+                    stream.WriteByte(0xDA);
+                    break;
+                case CompressionLevel.DefaultCompression:
+                    stream.WriteByte(0x9C);
+                    break;
+                case CompressionLevel.NoCompression:
+                    stream.WriteByte(0x01);
+                    break;
+                case CompressionLevel.UnknownCompression:
+                    throw new Exception("Unknown compression can't be written.");
             }
         }
     }
