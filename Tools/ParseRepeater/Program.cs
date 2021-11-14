b969716a583ebcc950d2e6d1559eab0667a40242
Petr Pivoňka <petrpiv1@gmail.com>
2021-11-14 05:21:46 +0100

    Add ParseRepeater tool

---
+++ Tools/ParseRepeater/Program.cs
@@ -0,0 +1,21 @@
+﻿using GBX.NET;
+
+if (args.Length == 0)
+    return;
+
+var fileName = args[0];
+
+while (true)
+{
+    try
+    {
+        GameBox.Parse(fileName);
+    }
+    catch(Exception ex)
+    {
+        Console.WriteLine(ex);
+    }
+
+    Console.WriteLine();
+    Console.ReadKey(true);
+}
