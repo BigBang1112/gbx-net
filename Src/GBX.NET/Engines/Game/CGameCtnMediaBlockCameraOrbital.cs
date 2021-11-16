3b880a9793cbd7e04e1be73d3f671fc007d3fa59
Petr Pivoňka <petrpiv1@gmail.com>
2021-11-16 20:21:53 +0100

    Add rough CGameCtnMediaBlockCameraOrbital support

---
+++ Src/GBX.NET/Engines/Game/CGameCtnMediaBlockCameraOrbital.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBX.NET.Engines.Game;
+
+[Node(0x030A0000)]
+public class CGameCtnMediaBlockCameraOrbital : CGameCtnMediaBlock
+{
+    private CGameCtnMediaBlockCameraOrbital()
+    {
+
+    }
+
+    [Chunk(0x030A0001)]
+    [AutoReadWriteChunk]
+    public class Chunk030A0001 : Chunk<CGameCtnMediaBlockCameraOrbital>
+    {
+        
+    }
+}
