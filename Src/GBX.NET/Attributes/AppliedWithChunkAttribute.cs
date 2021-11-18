5dc921d956007d8223bf733074772240eb33ebe8
Petr Pivoňka <petrpiv1@gmail.com>
2021-11-18 02:23:35 +0100

    Add AppliedWithChunkAttribute

---
+++ Src/GBX.NET/Attributes/AppliedWithChunkAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace GBX.NET.Attributes;
+
+[AttributeUsage(AttributeTargets.Property)]
+public class AppliedWithChunkAttribute : Attribute
+{
+    public Type ChunkType { get; }
+
+    public AppliedWithChunkAttribute(Type chunkType)
+    {
+        ChunkType = chunkType;
+    }
+}
