a9b4468f4b2c9192e10b4dec15374774224a65df
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-18 02:05:53 +0100

    Add CPlugBitmapAddress, CPlugBitmapSampler

---
+++ Src/GBX.NET/Engines/Plug/CPlugBitmapAddress.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBX.NET.Engines.Plug;
+
+[Node(0x09047000)]
+public class CPlugBitmapAddress : CPlugBitmapSampler
+{
+    protected CPlugBitmapAddress()
+    {
+
+    }
+
+    [Chunk(0x09047007)]
+    public class Chunk09047007 : Chunk<CPlugBitmapAddress>
+    {
+        public int U01;
+        public int U02;
+        public byte U03;
+
+        public override void ReadWrite(CPlugBitmapAddress n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref U01);
+            rw.Int32(ref U02);
+            rw.Byte(ref U03);
+        }
+    }
+
+    [Chunk(0x09047008)]
+    public class Chunk09047008 : Chunk<CPlugBitmapAddress>
+    {
+        public int U01;
+        public float U02;
+
+        public override void ReadWrite(CPlugBitmapAddress n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref U01); // ArchiveCountAndElems SMapFxDescOld/SBitmapElemToPack count*0x14
+            rw.Single(ref U02);
+        }
+    }
+}
---
+++ Src/GBX.NET/Engines/Plug/CPlugBitmapSampler.cs
@@ -0,0 +1,27 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+[Node(0x0907E000)]
+public class CPlugBitmapSampler : CPlug
+{
+    protected CPlugBitmapSampler()
+    {
+
+    }
+
+    [Chunk(0x0907E005)]
+    public class Chunk0907E005 : Chunk<CPlugBitmapSampler>
+    {
+        public string? U01;
+        public CMwNod? U02;
+        public int? U03;
+        public float? U04;
+
+        public override void ReadWrite(CPlugBitmapSampler n, GameBoxReaderWriter rw)
+        {
+            rw.Id(ref U01);
+            rw.NodeRef(ref U02);
+            rw.Int32(ref U03);
+            rw.Single(ref U04);
+        }
+    }
+}
