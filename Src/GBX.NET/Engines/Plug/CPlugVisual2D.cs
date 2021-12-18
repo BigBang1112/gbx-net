9aa2a9a57dc6cbb6216b7ff7c794dcf7f5ca42b6
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-18 02:05:20 +0100

    Add CPlugVisualIndexedLines, CPlugVisual2D, CPlugVisualQuads2D

---
+++ Src/GBX.NET/Engines/Plug/CPlugVisual2D.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBX.NET.Engines.Plug;
+
+[Node(0x0904A000)]
+public class CPlugVisual2D : CPlugVisual
+{
+    protected CPlugVisual2D()
+    {
+
+    }
+
+    [Chunk(0x0904A000)]
+    public class Chunk0904A000 : Chunk<CPlugVisual2D>
+    {
+        public float[]? U01;
+
+        public override void Read(CPlugVisual2D n, GameBoxReader r)
+        {
+            U01 = r.ReadArray<float>(r.ReadInt32() * 8);
+        }
+
+        public override void Write(CPlugVisual2D n, GameBoxWriter w)
+        {
+            if (U01 is null)
+            {
+                w.Write(0);
+                return;
+            }
+
+            w.Write(U01.Length / 8);
+            w.WriteArray_NoPrefix(U01);
+        }
+    }
+}
---
+++ Src/GBX.NET/Engines/Plug/CPlugVisualIndexedLines.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GBX.NET.Engines.Plug;
+
+[Node(0x09009000)]
+public class CPlugVisualIndexedLines : CPlugVisualIndexed
+{
+    protected CPlugVisualIndexedLines()
+    {
+
+    }
+
+    [Chunk(0x09009001)]
+    public class Chunk09009001 : Chunk<CPlugVisualIndexedLines>
+    {
+        public int U01;
+
+        public override void ReadWrite(CPlugVisualIndexedLines n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref U01);
+        }
+    }
+}
