437e76b359b3ffe41a4ca3b965ab8d9d48a1a653
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-22 03:40:48 +0100

    Add partial texture.gbx support for TMS/ESWC

---
+++ Src/GBX.NET/Engines/Plug/CPlugFileGen.cs
@@ -0,0 +1,10 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+[Node(0x0902F000)]
+public class CPlugFileGen : CPlugFileImg
+{
+    protected CPlugFileGen()
+    {
+
+    }
+}
---
+++ Src/GBX.NET/Engines/Plug/CPlugFileImg.cs
@@ -0,0 +1,10 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+[Node(0x09025000)]
+public class CPlugFileImg : CPlugFile
+{
+    protected CPlugFileImg()
+    {
+
+    }
+}
