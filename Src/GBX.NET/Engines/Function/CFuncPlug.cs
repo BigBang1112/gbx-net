7a9ecc4d69e25fd65fea8ef37c3aefe890a56156
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-30 02:11:20 +0100

    Describe CFuncPlug

--- Src/GBX.NET/Engines/Function/CFuncPlug.cs
+++ Src/GBX.NET/Engines/Function/CFuncPlug.cs
@@ -3,6 +3,35 @@
 [Node(0x0500B000)]
 public abstract class CFuncPlug : CFunc
 {
+    private float period;
+    private float phase;
+    private bool autoCreateMotion;
+    private bool randomizePhase;
+
+    public float Period
+    {
+        get => period;
+        set => period = value;
+    }
+
+    public float Phase
+    {
+        get => phase;
+        set => phase = value;
+    }
+
+    public bool AutoCreateMotion
+    {
+        get => autoCreateMotion;
+        set => autoCreateMotion = value;
+    }
+
+    public bool RandomizePhase
+    {
+        get => randomizePhase;
+        set => randomizePhase = value;
+    }
+
     protected CFuncPlug()
     {
 
@@ -11,17 +40,12 @@ public abstract class CFuncPlug : CFunc
     [Chunk(0x0500B004)]
     public class Chunk0500B004 : Chunk<CFuncPlug>
     {
-        public float U01;
-        public float U02;
-        public bool U03;
-        public bool U04;
-
         public override void ReadWrite(CFuncPlug n, GameBoxReaderWriter rw)
         {
-            rw.Single(ref U01);
-            rw.Single(ref U02);
-            rw.Boolean(ref U03);
-            rw.Boolean(ref U04);
+            rw.Single(ref n.period);
+            rw.Single(ref n.phase);
+            rw.Boolean(ref n.autoCreateMotion);
+            rw.Boolean(ref n.randomizePhase);
         }
     }
 }
