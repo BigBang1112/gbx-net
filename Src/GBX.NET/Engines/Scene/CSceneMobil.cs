55372c7b53cc2f26b7d737c45ccca88a01f11b22
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-30 05:38:59 +0100

    Fix CSceneMobil for TMS

--- Src/GBX.NET/Engines/Scene/CSceneMobil.cs
+++ Src/GBX.NET/Engines/Scene/CSceneMobil.cs
@@ -23,6 +23,17 @@ public class CSceneMobil : CSceneObject
 
         public override void ReadWrite(CSceneMobil n, GameBoxReaderWriter rw)
         {
+            rw.Int32(ref U01); // CSceneObject array?
+        }
+    }
+
+    [Chunk(0x0A011004)]
+    public class Chunk0A011004 : Chunk<CSceneMobil>
+    {
+        public int U01;
+
+        public override void ReadWrite(CSceneMobil n, GameBoxReaderWriter rw)
+        {
             rw.Int32(ref U01);
         }
     }
@@ -33,20 +44,6 @@ public class CSceneMobil : CSceneObject
         public override void Read(CSceneMobil n, GameBoxReader r, ILogger? logger)
         {
             n.item = Parse<CHmsItem>(r, 0x06003000, progress: null, logger)!;
-            var gds = r.ReadBytes(14);
-            var gdshsd = r.ReadInt32();
-            var str = r.ReadString();
-            var gdshsd1 = r.ReadInt32();
-            var gdshsd2 = r.ReadInt32();
-            var gdshsd3 = r.ReadInt32();
-            var gdshsd4 = r.ReadInt32();
-            var str2 = r.ReadString();
-            var gdshsd5 = r.ReadInt32();
-            var gdshsd6 = r.ReadInt32();
-            var gdshsd7 = r.ReadInt32();
-            var gdshsd8 = r.ReadInt32();
-            var str3 = r.ReadString();
-            var dgsg = r.ReadArray<int>(17);
         }
     }
 
