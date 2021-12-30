97bf0d865e8cf2ac120dfc2d686c3d40ecb9a77b
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-31 00:37:37 +0100

    Add CPlugShaderGeneric 0x001

--- Src/GBX.NET/Engines/Plug/CPlugShaderGeneric.cs
+++ Src/GBX.NET/Engines/Plug/CPlugShaderGeneric.cs
@@ -8,6 +8,17 @@ public abstract class CPlugShaderGeneric : CPlugShader
 
     }
 
+    [Chunk(0x09004001)]
+    public class Chunk09004001 : Chunk<CPlugShaderGeneric>
+    {
+        public float[]? U01;
+
+        public override void ReadWrite(CPlugShaderGeneric n, GameBoxReaderWriter rw)
+        {
+            rw.Array<float>(ref U01, count: 22);
+        }
+    }
+
     [Chunk(0x09004003)]
     public class Chunk09004003 : Chunk<CPlugShaderGeneric>
     {
