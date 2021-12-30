3c9165d14807d919125ae364fb7300257bceed5c
Petr Pivoňka <petrpiv1@gmail.com>
2021-12-31 00:34:24 +0100

    Add CFuncTreeRotate 0x001

---
+++ Src/GBX.NET/Engines/Function/CFuncTreeRotate.cs
@@ -0,0 +1,42 @@
+﻿namespace GBX.NET.Engines.Function;
+
+[Node(0x0501E000)]
+public class CFuncTreeRotate : CFuncTree
+{
+    private float angleMin;
+    private float angleMax;
+
+    public float AngleMin
+    {
+        get => angleMin;
+        set => angleMin = value;
+    }
+
+    public float AngleMax
+    {
+        get => angleMax;
+        set => angleMax = value;
+    }
+
+    protected CFuncTreeRotate()
+    {
+
+    }
+
+    [Chunk(0x0501E001)]
+    public class Chunk0501E001 : Chunk<CFuncTreeRotate>
+    {
+        public float U01;
+        public float U02;
+        public float U03;
+
+        public override void ReadWrite(CFuncTreeRotate n, GameBoxReaderWriter rw)
+        {
+            rw.Single(ref U01);
+            rw.Single(ref U02);
+            rw.Single(ref U03);
+            rw.Single(ref n.angleMin);
+            rw.Single(ref n.angleMax);
+        }
+    }
+}
