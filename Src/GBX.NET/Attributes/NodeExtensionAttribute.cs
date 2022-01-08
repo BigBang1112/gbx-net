61d3ae7a1389bccde6980584d25396fa18bd3482
Petr Pivoňka <petrpiv1@gmail.com>
2022-01-08 02:53:43 +0100

    Add NodeExtension attribute

---
+++ Src/GBX.NET/Attributes/NodeExtensionAttribute.cs
@@ -0,0 +1,12 @@
+﻿namespace GBX.NET.Attributes;
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = true)]
+public class NodeExtensionAttribute : Attribute
+{
+    public string Extension { get; }
+
+    public NodeExtensionAttribute(string extension)
+    {
+        Extension = extension;
+    }
+}
