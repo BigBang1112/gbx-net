62d9dae500ce1d58c742e289d756a4f757084d53
Petr Pivoňka <petrpiv1@gmail.com>
2022-01-10 23:11:23 +0100

    Add NodeCacheDefineMappingsBenchmark

---
+++ Benchmarks/GBX.NET.Benchmarks/NodeCacheDefineMappingsBenchmark.cs
@@ -0,0 +1,22 @@
+﻿using BenchmarkDotNet.Attributes;
+
+namespace GBX.NET.Benchmarks;
+
+[MemoryDiagnoser]
+[CustomBenchmark]
+public class NodeCacheDefineMappingsBenchmark : Benchmark
+{
+    public Dictionary<uint, uint> Mappings { get; set; } = new();
+
+    [Benchmark]
+    public void DefineMappings()
+    {
+        NodeCacheManager.DefineMappings(Mappings);
+    }
+
+    [Benchmark(Baseline = true)]
+    public void DefineMappings2()
+    {
+        NodeCacheManager.DefineMappings2(Mappings);
+    }
+}
