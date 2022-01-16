3df564527dbbb3945d95f0179a796989fe8d8620
Petr Pivoňka <petrpiv1@gmail.com>
2022-01-16 02:18:35 +0100

    Make Quaternion readonly record struct

--- Src/GBX.NET/Quaternion.cs
+++ Src/GBX.NET/Quaternion.cs
@@ -1,20 +1,7 @@
 ﻿namespace GBX.NET;
 
-public struct Quaternion
+public readonly record struct Quaternion(float X, float Y, float Z, float W)
 {
-    public float X { get; }
-    public float Y { get; }
-    public float Z { get; }
-    public float W { get; }
-
-    public Quaternion(float x, float y, float z, float w)
-    {
-        X = x;
-        Y = y;
-        Z = z;
-        W = w;
-    }
-
     public Quaternion(Vec3 xyz, float w) : this(xyz.X, xyz.Y, xyz.Z, w)
     {
 
@@ -29,12 +16,7 @@ public struct Quaternion
         return (pitch, yaw, roll);
     }
 
-    public override string ToString() => $"({X}, {Y}, {Z}, {W})";
-    public override int GetHashCode() => X.GetHashCode() ^ Y.GetHashCode() ^ Z.GetHashCode() ^ W.GetHashCode();
-    public override bool Equals(object? obj) => obj is Quaternion a && a == this;
-
-    public static bool operator ==(Quaternion a, Quaternion b) => a.X == b.X && a.Y == b.Y && a.Z == b.Z && a.W == b.W;
-    public static bool operator !=(Quaternion a, Quaternion b) => !(a.X == b.X && a.Y == b.Y && a.Z == b.Z && a.W == b.W);
+    public static readonly Quaternion Zero = new();
 
     public static Quaternion operator +(Quaternion a, Quaternion b) => new(a.X + b.X, a.Y + b.Y, a.Z + b.Z, a.W + b.W);
     public static Quaternion operator -(Quaternion a) => new(-a.X, -a.Y, -a.Z, -a.W);
