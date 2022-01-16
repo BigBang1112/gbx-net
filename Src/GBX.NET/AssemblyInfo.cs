6fd932f4b1658db5269413097bc4e2906125a96b
Petr Pivoňka <petrpiv1@gmail.com>
2022-01-16 02:19:09 +0100

    Move InternalsVisibleTo to csproj

--- Src/GBX.NET/AssemblyInfo.cs
+++
@@ -1,5 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("GBX.NET.Tests")]
-[assembly: InternalsVisibleTo("GBX.NET.PAK")]
-[assembly: InternalsVisibleTo("GBX.NET.Benchmarks")]
