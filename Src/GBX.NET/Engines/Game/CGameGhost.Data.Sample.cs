ac19e19bd565690099b6dc5aa9ad3dd318663121
Petr Pivoňka <petrpiv1@gmail.com>
2022-01-22 21:37:46 +0100

    Separate CGameGhost

---
+++ Src/GBX.NET/Engines/Game/CGameGhost.Data.Sample.cs
@@ -0,0 +1,36 @@
+﻿namespace GBX.NET.Engines.Game;
+
+public partial class CGameGhost
+{
+    public partial class Data
+    {
+        public class Sample : NET.Sample
+        {
+            private Data? owner;
+
+            public Sample(byte[] data) : base(data)
+            {
+                
+            }
+
+            internal void AssignTo(Data? ghostData)
+            {
+                owner = ghostData;
+
+                if (owner == null || owner.samplePeriod.TotalMilliseconds <= 0)
+                {
+                    Timestamp = null;
+                    return;
+                }
+
+                UpdateTimestamp();
+            }
+
+            internal void UpdateTimestamp()
+            {
+                if (owner is not null)
+                    Timestamp = TimeSpan.FromMilliseconds(owner.samplePeriod.TotalMilliseconds * owner.Samples.IndexOf(this));
+            }
+        }
+    }
+}
