0554a0f5cba42aaf74e597c9f29bad30ebeac6b7
Petr Pivoňka <petrpiv1@gmail.com>
2022-01-22 04:00:42 +0100

    Specify event ids

--- Src/GBX.NET/LoggerMessageGen.cs
+++ Src/GBX.NET/LoggerMessageGen.cs
@@ -2,9 +2,9 @@
 
 internal static partial class LoggerMessageGen
 {
-    [LoggerMessage(Level = LogLevel.Debug, Message = "0x{chunkHex} ({progressPercentage}%)", EventId = 0)]
+    [LoggerMessage(Level = LogLevel.Debug, Message = "0x{chunkHex} ({progressPercentage}%)", EventId = 1)]
     public static partial void LogChunkProgress(this ILogger logger, string chunkHex, float progressPercentage);
 
-    [LoggerMessage(Level = LogLevel.Debug, Message = "DONE! ({time}ms)", EventId = 0)]
+    [LoggerMessage(Level = LogLevel.Debug, Message = "DONE! ({time}ms)", EventId = 2)]
     public static partial void LogNodeComplete(this ILogger logger, double time);
 }
