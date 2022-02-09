6ad29b1043cbc8ec0520bd8e969bbf21f1cd6d2f
Petr Pivoňka <petrpiv1@gmail.com>
2022-02-09 18:22:04 +0100

    Add GbxReadWriteEquality

---
+++ Tools/GbxReadWriteEquality/Program.cs
@@ -0,0 +1,35 @@
+﻿using GBX.NET;
+using Microsoft.Extensions.Logging;
+
+if (args.Length == 0) return;
+
+var logger = LoggerFactory.Create(builder =>
+{
+    builder.AddSimpleConsole(options =>
+    {
+        options.IncludeScopes = true;
+        options.SingleLine = true;
+        options.TimestampFormat = "yyyy-MM-dd HH:mm:ss ";
+    });
+    builder.SetMinimumLevel(LogLevel.Debug);
+}).CreateLogger<Program>();
+
+var fileName = args[0];
+
+var rootPath = Path.GetDirectoryName(typeof(Program).Assembly.Location) + "/";
+
+GameBox.Decompress(fileName, rootPath + Path.GetFileNameWithoutExtension(fileName) + "-decompressed" + Path.GetExtension(fileName));
+var node = GameBox.ParseNode(fileName, logger: logger);
+
+if (node is null)
+{
+    return;
+}
+
+using var ms = new MemoryStream();
+
+node.Save(ms);
+
+ms.Position = 0;
+
+GameBox.Decompress(ms, rootPath + Path.GetFileNameWithoutExtension(fileName) + "-saved-decompressed" + Path.GetExtension(fileName));
