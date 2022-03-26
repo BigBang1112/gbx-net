e272a4882b36bf0d12e14d093e74d58b5f0ed09d
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-27 00:17:12 +0100

    Add surface materials

--- Src/GBX.NET/Engines/Plug/CPlugSurface.cs
+++ Src/GBX.NET/Engines/Plug/CPlugSurface.cs
@@ -5,18 +5,29 @@
 public class CPlugSurface : CPlug
 {
     private CPlugSurfaceGeom? geom;
+    private SurfaceMaterial[]? materials;
 
     public CPlugSurfaceGeom? Geom { get => geom; set => geom = value; }
+
+    public SurfaceMaterial[]? Materials { get => materials; set => materials = value; }
 
     protected CPlugSurface()
     {
 
     }
 
+    public class SurfaceMaterial // CPlugMaterial
+    {
+        public bool containsMaterial;
+        public CMwNod? material;
+        public ushort? defaultMaterialIndex; // set if containsMaterial is false
+    }
+
     [Chunk(0x0900C000)]
     public class Chunk0900C000 : Chunk<CPlugSurface>
     {
         public string? U01;
+        
 
         public override void ReadWrite(CPlugSurface n, GameBoxReaderWriter rw)
         {
@@ -27,6 +38,22 @@ public class CPlugSurface : CPlug
             }
 
             rw.NodeRef(ref n.geom);
+
+            rw.Array(n.materials, (rw, x) =>
+            {
+                rw.Boolean(ref x.containsMaterial);
+
+                if (x.containsMaterial)
+                {
+                    rw.NodeRef(x.material);
+                }
+                else
+                {
+                    rw.UInt16(x.defaultMaterialIndex);
+
+                    // sometimes there could be node ref?
+                }
+            });
         }
     }
 
