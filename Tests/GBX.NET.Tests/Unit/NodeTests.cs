6bd446a85ea798d779cb53ae16d5b66a12e37034
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-28 01:17:54 +0200

    Add GetChunk test

--- Tests/GBX.NET.Tests/Unit/NodeTests.cs
+++ Tests/GBX.NET.Tests/Unit/NodeTests.cs
@@ -1,4 +1,5 @@
 ﻿using GBX.NET.Engines.Game;
+using System;
 using Xunit;
 
 namespace GBX.NET.Tests.Unit;
@@ -41,5 +42,34 @@ public class NodeTests
         var gbx = node.ToGbx<CGameCtnMediaBlockSound>();
 
         Assert.Equal(node, gbx.Node);
+    }
+
+    // test GetChunk
+
+    [Fact]
+    public void GetChunk_ChunkIsNotNull_ReturnsChunk()
+    {
+        var node = CGameCtnMediaBlockSound.Create().ForTM2().Build();
+        var chunk = node.GetChunk<CGameCtnMediaBlockSound.Chunk030A7004>();
+
+        Assert.NotNull(chunk);
+    }
+
+    [Fact]
+    public void GetChunk_ChunkIsNull_ReturnsNull()
+    {
+        var node = CGameCtnMediaBlockSound.Create().ForTM2().Build();
+        var chunk = node.GetChunk<CGameCtnMediaBlockSound.Chunk030A7001>();
+
+        Assert.Null(chunk);
+    }
+
+    [Fact]
+    public void GetChunk_ChunkIsNotNull_ReturnsChunkOfType()
+    {
+        var node = CGameCtnMediaBlockSound.Create().ForTM2().Build();
+        var chunk = node.GetChunk<CGameCtnMediaBlockSound.Chunk030A7004>();
+
+        Assert.IsType<CGameCtnMediaBlockSound.Chunk030A7004>(chunk);
     }
 }
