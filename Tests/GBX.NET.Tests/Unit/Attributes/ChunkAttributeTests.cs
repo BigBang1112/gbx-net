55a37c22d2a3f09059dbcbd6155ae4ce4ae949c3
Petr Pivoňka <petrpiv1@gmail.com>
2022-03-28 22:08:25 +0200

    Add ChunkAttribute tests

---
+++ Tests/GBX.NET.Tests/Unit/Attributes/ChunkAttributeTests.cs
@@ -0,0 +1,35 @@
+﻿using GBX.NET.Attributes;
+using Xunit;
+
+namespace GBX.NET.Tests.Unit.Attributes;
+
+public class ChunkAttributeTests
+{
+    // test GetClassPart, should truncate last 3 digits
+    [Fact]
+    public void GetClassPart_ShouldTruncateLast3Digits()
+    {
+        // arrange
+        var chunkAttribute = new ChunkAttribute(0x0304301F);
+
+        // act
+        var result = chunkAttribute.GetClassPart();
+
+        // assert
+        Assert.Equal<uint>(expected: 0x03043000, actual: result);
+    }
+
+    // test GetChunkPart
+    [Fact]
+    public void GetChunkPart_ShouldReturnLast3Digits()
+    {
+        // arrange
+        var chunkAttribute = new ChunkAttribute(0x0304301F);
+
+        // act
+        var result = chunkAttribute.GetChunkPart();
+
+        // assert
+        Assert.Equal<uint>(expected: 0x01F, actual: result);
+    }
+}
