ab59cd7318b1fbb6b37fe3f97f0198b49354a295
Petr Pivoňka <petrpiv1@gmail.com>
2022-04-04 18:45:07 +0200

    Add CGameCtnMediaBlockOpponentVisibility

---
+++ Src/GBX.NET/Engines/Game/CGameCtnMediaBlockOpponentVisibility.EVisibility.cs
@@ -0,0 +1,11 @@
+﻿namespace GBX.NET.Engines.Game;
+
+public partial class CGameCtnMediaBlockOpponentVisibility
+{
+    public enum EVisibility
+    {
+        Hidden,
+        Ghost,
+        Opaque
+    }
+}
