d80448114711ddb5a2a4e35fc51253bcf57f0f63
Petr Pivoňka <petrpiv1@gmail.com>
2022-06-16 23:29:08 +0200

    Use CPlugTreeLight from external node

--- Src/GBX.NET/Engines/Plug/CPlugTreeLight.cs
+++ Src/GBX.NET/Engines/Plug/CPlugTreeLight.cs
@@ -4,10 +4,11 @@
 public class CPlugTreeLight : CPlugTree
 {
     private CPlugLight? plugLight;
+    private int? plugLightIndex;
 
     public CPlugLight? PlugLight
     {
-        get => plugLight;
+        get => plugLight = GetNodeFromRefTable(plugLight, plugLightIndex) as CPlugLight;
         set => plugLight = value;
     }
 
@@ -21,7 +22,7 @@ public class CPlugTreeLight : CPlugTree
     {
         public override void ReadWrite(CPlugTreeLight n, GameBoxReaderWriter rw)
         {
-            rw.NodeRef(ref n.plugLight);
+            rw.NodeRef(ref n.plugLight, ref n.plugLightIndex);
         }
     }
 }
