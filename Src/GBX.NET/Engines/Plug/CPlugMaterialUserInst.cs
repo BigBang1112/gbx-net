1fb37cd8ac640b084dd8c2520803867714e6cce3
Petr Pivoňka <petrpiv1@gmail.com>
2022-06-24 01:43:33 +0200

    Update CPlugMaterialUserInst

--- Src/GBX.NET/Engines/Plug/CPlugMaterialUserInst.cs
+++ Src/GBX.NET/Engines/Plug/CPlugMaterialUserInst.cs
@@ -4,13 +4,43 @@
 [NodeExtension("Mat")]
 public class CPlugMaterialUserInst : CMwNod
 {
-    private string? materialFile;
+    private string? model;
+    private string? baseTexture;
+    private string? link;
+    private Cst[]? csts;
+    private UvAnim[]? uvAnims;
+    private string? hidingGroup;
+    private int tilingU;
+    private int tilingV;
+    private float textureSizeInMeters;
+    private bool isNatural;
 
-    public string? MaterialFile
-    {
-        get => materialFile;
-        set => materialFile = value;
-    }
+    [NodeMember(ExactlyNamed = true)]
+    public string? Model { get => model; set => model = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public string? BaseTexture { get => baseTexture; set => baseTexture = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public string? Link { get => link; set => link = value; }
+
+    public Cst[]? Csts { get => csts; set => csts = value; }
+    public UvAnim[]? UvAnims { get => uvAnims; set => uvAnims = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public string? HidingGroup { get => hidingGroup; set => hidingGroup = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public int TilingU { get => tilingU; set => tilingU = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public int TilingV { get => tilingV; set => tilingV = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public float TextureSizeInMeters { get => textureSizeInMeters; set => textureSizeInMeters = value; }
+
+    [NodeMember(ExactlyNamed = true)]
+    public bool IsNatural { get => isNatural; set => isNatural = value; }
 
     protected CPlugMaterialUserInst()
     {
@@ -19,7 +49,7 @@ public class CPlugMaterialUserInst : CMwNod
 
     public override string ToString()
     {
-        return $"{base.ToString()} {{ {MaterialFile ?? "No material file"} }}";
+        return $"{base.ToString()} {{ {Link ?? "No material file"} }}";
     }
 
     [Chunk(0x090FD000)]
@@ -27,14 +57,17 @@ public class CPlugMaterialUserInst : CMwNod
     {
         private int version;
 
-        public CMwNod? U01;
-        public CMwNod? U02;
-        public int U03;
+        public string? U01;
+        public string? U02;
+        public string? U03;
         public byte U04;
-        public CMwNod? U05;
+        public byte? U05;
+        public ulong[]? U06;
+        public string[]? U07;
+        public string? U08;
 
         /// <summary>
-        /// Version 10: TM®, version 8/9: ManiaPlanet
+        /// Version 10: TM®, version 9: ManiaPlanet 2019.11.19.1850
         /// </summary>
         public int Version
         {
@@ -45,24 +78,81 @@ public class CPlugMaterialUserInst : CMwNod
         public override void ReadWrite(CPlugMaterialUserInst n, GameBoxReaderWriter rw)
         {
             rw.Int32(ref version);
-            rw.NodeRef(ref U01);
-            rw.NodeRef(ref U02);
-            rw.Int32(ref U03);
+
+            rw.Id(ref U01);
+            rw.Id(ref n.model);
+            rw.String(ref n.baseTexture);
             rw.Byte(ref U04);
-            if (Version < 9)
-                rw.Id(ref n.materialFile);
-            if (Version >= 9)
+
+            if (version >= 10)
             {
-                if (Version >= 10)
-                    rw.Byte();
-                rw.String(ref n.materialFile);
+                rw.Byte(ref U05);
             }
-            rw.Int32();
-            rw.Int32();
-            rw.Int32();
-            rw.Int32();
-            rw.Int32();
-            rw.NodeRef(ref U05);
+
+            if (version >= 1)
+            {
+                if (version < 9)
+                {
+                    rw.Id(ref n.link);
+                }
+                else
+                {
+                    rw.String(ref n.link);
+                }
+
+                if (version >= 11)
+                {
+                    // Something
+                }
+
+                if (version >= 2)
+                {
+                    rw.Array<Cst>(ref n.csts, r => new(
+                        U01: r.ReadId(),
+                        U02: r.ReadId(),
+                        U03: r.ReadInt32()
+                    ), (x, w) =>
+                    {
+                        w.WriteId(x.U01);
+                        w.WriteId(x.U02);
+                        w.Write(x.U03);
+                    });
+
+                    rw.Array<ulong>(ref U06);
+
+                    if (version >= 3)
+                    {
+                        rw.Array<UvAnim>(ref n.uvAnims, r => new(
+                            U01: r.ReadId(),
+                            U02: r.ReadId(),
+                            U03: r.ReadSingle(),
+                            U04: r.ReadUInt64()
+                        ), (x, w) =>
+                        {
+                            w.WriteId(x.U01);
+                            w.WriteId(x.U02);
+                            w.Write(x.U03);
+                            w.Write(x.U04);
+                        });
+
+                        if (version >= 4)
+                        {
+                            rw.ArrayId(ref U07);
+
+                            if (version >= 6)
+                            {
+                                // UserTextures
+                                rw.Int32();
+
+                                if (version >= 7)
+                                {
+                                    rw.Id(ref n.hidingGroup);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
         }
     }
 
@@ -72,6 +162,7 @@ public class CPlugMaterialUserInst : CMwNod
         private int version;
 
         public CMwNod? U01;
+        public int? U02;
 
         public int Version
         {
@@ -83,11 +174,23 @@ public class CPlugMaterialUserInst : CMwNod
         {
             rw.Int32(ref version);
             rw.NodeRef(ref U01);
-            rw.Int32();
-            rw.Int32();
-            rw.Single();
-            rw.Int32();
-            rw.Int32();
+
+            if (version >= 3)
+            {
+                rw.Int32(ref n.tilingU);
+                rw.Int32(ref n.tilingV);
+                rw.Single(ref n.textureSizeInMeters);
+
+                if (version >= 4)
+                {
+                    rw.Int32(ref U02);
+
+                    if (version >= 5)
+                    {
+                        rw.Boolean(ref n.isNatural);
+                    }
+                }
+            }
         }
     }
 
@@ -100,4 +203,7 @@ public class CPlugMaterialUserInst : CMwNod
             rw.Int32();
         }
     }
+
+    public record Cst(string U01, string U02, int U03);
+    public record UvAnim(string U01, string U02, float U03, ulong U04);
 }
