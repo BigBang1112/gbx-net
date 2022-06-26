b0054ef4fe1ea249283b439799f3762e8e6404a1
Petr Pivoňka <petrpiv1@gmail.com>
2022-06-26 02:01:56 +0200

    Add WIP CPlugSolid2Model

---
+++ Src/GBX.NET/Engines/Plug/CPlugSolid2Model.cs
@@ -0,0 +1,40 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+[Node(0x090BB000)]
+public class CPlugSolid2Model : CMwNod
+{
+    protected CPlugSolid2Model()
+    {
+        
+    }
+
+    #region Chunks
+
+    #region 0x000 chunk
+
+    /// <summary>
+    /// CPlugSolid2Model 0x000 chunk
+    /// </summary>
+    [Chunk(0x090BB000), IgnoreChunk]
+    public class Chunk090BB000 : Chunk<CPlugSolid2Model>, IVersionable
+    {
+        private int version;
+
+        public string? U01;
+        public int U02;
+
+        public int Version { get => version; set => version = value; }
+
+        public override void ReadWrite(CPlugSolid2Model n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref version);
+            rw.Id(ref U01);
+            rw.Int32(ref U02); // SShadedGeom array
+            // ...
+        }
+    }
+
+    #endregion
+
+    #endregion
+}
