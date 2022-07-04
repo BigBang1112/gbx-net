4bed8e893770f00e60eb963d6596b15d42eefaea
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-04 20:42:12 +0200

    Adjust ExportToObj methods

--- Src/GBX.NET/Engines/Plug/CPlugCrystal.cs
+++ Src/GBX.NET/Engines/Plug/CPlugCrystal.cs
@@ -71,9 +71,15 @@ public class CPlugCrystal : CPlugTreeGenerator
 
     #region Methods
 
-    public void ExportToObj(Stream objStream, Stream mtlStream)
+    /// <summary>
+    /// Exports the crystal to .obj file.
+    /// </summary>
+    /// <param name="objStream">Stream to write OBJ content into.</param>
+    /// <param name="mtlStream">Stream to write MTL content into.</param>
+    /// <param name="gameDataFolderPath">Folder for the Material.Gbx, Texture.Gbx, and .dds lookup.</param>
+    public void ExportToObj(Stream objStream, Stream mtlStream, string gameDataFolderPath)
     {
-        using var exporter = new ObjFileExporter(objStream, mtlStream);
+        using var exporter = new ObjFileExporter(objStream, mtlStream, gameDataFolderPath);
 
         exporter.Export(this);
     }
--- Src/GBX.NET/Engines/Plug/CPlugSolid.cs
+++ Src/GBX.NET/Engines/Plug/CPlugSolid.cs
@@ -24,14 +24,20 @@ public class CPlugSolid : CPlug
 
     }
 
-    public void ExportToObj(Stream objStream, Stream mtlStream)
+    /// <summary>
+    /// Exports the solid to .obj file.
+    /// </summary>
+    /// <param name="objStream">Stream to write OBJ content into.</param>
+    /// <param name="mtlStream">Stream to write MTL content into.</param>
+    /// <param name="gameDataFolderPath">Folder for the Material.Gbx, Texture.Gbx, and .dds lookup.</param>
+    public void ExportToObj(Stream objStream, Stream mtlStream, string gameDataFolderPath)
     {
         if (Tree is not CPlugTree tree)
         {
             return;
         }
 
-        using var exporter = new ObjFileExporter(objStream, mtlStream);
+        using var exporter = new ObjFileExporter(objStream, mtlStream, gameDataFolderPath);
         
         exporter.Export(tree);
     }
