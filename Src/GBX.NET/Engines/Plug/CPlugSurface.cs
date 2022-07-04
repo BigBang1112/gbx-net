99d3b5a0c52db19519faf587e38724730d4e25d3
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-04 20:33:23 +0200

    Add unfinished CPlugSurface 0x003

--- Src/GBX.NET/Engines/Plug/CPlugSurface.cs
+++ Src/GBX.NET/Engines/Plug/CPlugSurface.cs
@@ -66,4 +66,70 @@ public class CPlugSurface : CPlug
             rw.Boolean(ref U01);
         }
     }
+
+    [Chunk(0x0900C003)]
+    public class Chunk0900C003 : Chunk<CPlugSurface>, IVersionable
+    {
+        private int version;
+
+        public int U01;
+        public int U02;
+        public int U03;
+        
+        public int U06;
+
+        public int Version { get => version; set => version = value; }
+
+        public override void ReadWrite(CPlugSurface n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref version);
+
+            if (version >= 2)
+            {
+                rw.Int32(ref U01);
+            }
+
+            rw.Int32(ref U02); // ArchiveGmSurf?
+
+            rw.Int32(ref U03); // ArchiveMaterials?
+
+            var wat = rw.Array<Vec3>();
+            
+            var nice = rw.Array(null, r =>
+            {
+                return (r.ReadSingle(),
+                    r.ReadSingle(),
+                    r.ReadSingle(),
+                    r.ReadSingle(),
+                    r.ReadInt3(),
+                    r.ReadUInt16(),
+                    r.ReadByte(),
+                    r.ReadByte());
+            }, (x, w) => { });
+
+            var U04 = rw.Int32();
+
+            var U05 = rw.Array(null, r =>
+            {
+                return (r.ReadVec3(),
+                    r.ReadVec3(),
+                    r.ReadInt32());
+            }, (x, w) => { });
+
+            /*var gdsg = rw.Reader.ReadArray<object>(r =>
+            {
+                var u01 = r.ReadBoolean();
+
+                if (!u01)
+                {
+                    var u02 = r.ReadInt16();
+                    var u03 = r.ReadInt32();
+                    var u04 = r.ReadUInt64();
+                    var nice = r.ReadNodeRef();
+                }
+
+                return null;
+            });*/
+        }
+    }
 }
