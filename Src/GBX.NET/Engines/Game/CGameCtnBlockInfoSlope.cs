8adbcc9e9978f31cf5b6981d76ea55d30e04f475
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-08 05:20:11 +0200

    Add CGameCtnBlockInfoSlope

---
+++ Src/GBX.NET/Engines/Game/CGameCtnBlockInfoSlope.cs
@@ -0,0 +1,10 @@
+﻿namespace GBX.NET.Engines.Game;
+
+[Node(0x03054000)]
+public class CGameCtnBlockInfoSlope : CGameCtnBlockInfo
+{
+    protected CGameCtnBlockInfoSlope()
+    {
+        
+    }
+}
