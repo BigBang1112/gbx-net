4592780965772748c7fffe110e45a6a84c929682
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-08 05:18:45 +0200

    Add CGameCtnBlockInfoClip 0x002

--- Src/GBX.NET/Engines/Game/CGameCtnBlockInfoClip.cs
+++ Src/GBX.NET/Engines/Game/CGameCtnBlockInfoClip.cs
@@ -6,8 +6,29 @@
 [NodeExtension("EDClip")]
 public class CGameCtnBlockInfoClip : CGameCtnBlockInfo
 {
+    private string? aSymmetricalClipId;
+
+    [NodeMember(ExactlyNamed = true)]
+    public string? ASymmetricalClipId { get => aSymmetricalClipId; set => aSymmetricalClipId = value; }
+
     protected CGameCtnBlockInfoClip()
     {
 
     }
+
+    #region 0x002 chunk
+
+    /// <summary>
+    /// CGameCtnBlockInfoClip 0x002 chunk
+    /// </summary>
+    [Chunk(0x03053002)]
+    public class Chunk03053002 : Chunk<CGameCtnBlockInfoClip>
+    {
+        public override void ReadWrite(CGameCtnBlockInfoClip n, GameBoxReaderWriter rw)
+        {
+            rw.Id(ref n.aSymmetricalClipId);
+        }
+    }
+
+    #endregion
 }
