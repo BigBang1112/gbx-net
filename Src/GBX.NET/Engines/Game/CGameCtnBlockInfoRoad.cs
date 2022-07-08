15e4ab785fc0661f9a801e60202b594516321c19
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-08 05:19:04 +0200

    Add CGameCtnBlockInfoRoad 0x000

--- Src/GBX.NET/Engines/Game/CGameCtnBlockInfoRoad.cs
+++ Src/GBX.NET/Engines/Game/CGameCtnBlockInfoRoad.cs
@@ -8,8 +8,30 @@
 [NodeExtension("EDRoad")]
 public class CGameCtnBlockInfoRoad : CGameCtnBlockInfo
 {
+    private CGameCtnBlockInfoSlope? slope;
+
+    [NodeMember(ExactlyNamed = true)]
+    public CGameCtnBlockInfoSlope? Slope { get => slope; set => slope = value; }
+
     protected CGameCtnBlockInfoRoad()
     {
 
     }
+
+
+    #region 0x000 chunk
+
+    /// <summary>
+    /// CGameCtnBlockInfoRoad 0x000 chunk
+    /// </summary>
+    [Chunk(0x03052000)]
+    public class Chunk03052000 : Chunk<CGameCtnBlockInfoRoad>
+    {
+        public override void ReadWrite(CGameCtnBlockInfoRoad n, GameBoxReaderWriter rw)
+        {
+            rw.NodeRef<CGameCtnBlockInfoSlope>(ref n.slope);
+        }
+    }
+
+    #endregion
 }
