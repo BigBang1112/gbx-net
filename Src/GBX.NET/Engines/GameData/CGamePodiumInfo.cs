143b839033aafe9ec75754d06d3aaf29265c54ee
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-11 00:21:34 +0200

    Move CGamePodiumInfo to modern ID

--- Src/GBX.NET/Engines/GameData/CGamePodiumInfo.cs
+++ Src/GBX.NET/Engines/GameData/CGamePodiumInfo.cs
@@ -1,26 +1,27 @@
-﻿namespace GBX.NET.Engines.Game;
+﻿namespace GBX.NET.Engines.GameData;
 
 /// <summary>
 /// Podium info.
 /// </summary>
-/// <remarks>ID: 0x03168000</remarks>
-[Node(0x03168000)]
+/// <remarks>ID: 0x2E035000</remarks>
+[Node(0x2E035000)]
 [NodeExtension("GamePodiumInfo")]
 public class CGamePodiumInfo : CMwNod
 {
-    private int[] mediaClipFids;
+    private ExternalNode<CMwNod>[] mediaClipFids;
 
-    public int[] MediaClipFids { get => mediaClipFids; set => mediaClipFids = value; }
+    [NodeMember(ExactlyNamed = true)]
+    public ExternalNode<CMwNod>[] MediaClipFids { get => mediaClipFids; set => mediaClipFids = value; }
 
     protected CGamePodiumInfo()
     {
-        mediaClipFids = null!;
+        mediaClipFids = Array.Empty<ExternalNode<CMwNod>>();
     }
 
     /// <summary>
     /// CGamePodiumInfo 0x000 chunk
     /// </summary>
-    [Chunk(0x03168000)]
+    [Chunk(0x2E035000)]
     public class Chunk03168000 : Chunk<CGamePodiumInfo>
     {
         public int U01;
@@ -28,7 +29,7 @@ public class CGamePodiumInfo : CMwNod
         public override void ReadWrite(CGamePodiumInfo n, GameBoxReaderWriter rw)
         {
             rw.Int32(ref U01);
-            rw.Array<int>(ref n.mediaClipFids!);
+            rw.ArrayNode<CMwNod>(ref n.mediaClipFids!);
         }
     }
 }
