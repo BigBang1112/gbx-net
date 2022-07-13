5ec130ca379843be43359c49b9c638834c4a78d3
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-13 18:02:14 +0200

    Add IconExporter

---
+++ Samples/IconExporter/Program.cs
@@ -0,0 +1,33 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.GameData;
+using GBX.NET.Imaging;
+
+if (args.Length == 0)
+{
+    return;
+}
+
+var fileName = args[0];
+
+string[] fileNames;
+
+if (File.Exists(fileName))
+{
+    fileNames = new[] { fileName };
+}
+else
+{
+    fileNames = Directory.GetFiles(Path.GetDirectoryName(fileName)!, "*.Gbx", SearchOption.AllDirectories);
+}
+
+foreach (var filePath in fileNames)
+{
+    var node = GameBox.ParseNodeHeader(filePath);
+
+    if (node is not CGameCtnCollector collector)
+    {
+        continue;
+    }
+
+    collector.ExportIcon(filePath + ".png");
+}
