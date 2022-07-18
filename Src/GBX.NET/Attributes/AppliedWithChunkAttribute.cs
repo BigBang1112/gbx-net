dd016d09c72645611b20f6a0a0af6526c36f4446
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-18 03:20:12 +0200

    Add SinceVersion and UpToVersion

--- Src/GBX.NET/Attributes/AppliedWithChunkAttribute.cs
+++ Src/GBX.NET/Attributes/AppliedWithChunkAttribute.cs
@@ -1,10 +1,30 @@
 ﻿namespace GBX.NET.Attributes;
 
+/// <summary>
+/// Tells which chunk is used to write a certain node memeber.
+/// </summary>
 [AttributeUsage(AttributeTargets.Property, AllowMultiple = true)]
 public class AppliedWithChunkAttribute : Attribute
 {
+    /// <summary>
+    /// Type of the chunk.
+    /// </summary>
     public Type ChunkType { get; }
 
+    /// <summary>
+    /// Since which version this member is written to the chunk. Ignored on chunks without <see cref="IVersionable"/>.
+    /// </summary>
+    public int SinceVersion { get; set; }
+
+    /// <summary>
+    /// Up to which version the member is written to the chunk. 0 (default) means there's no known version limit. Ignored on chunks without <see cref="IVersionable"/>.
+    /// </summary>
+    public int UpToVersion { get; set; }
+
+    /// <summary>
+    /// Creates the <see cref="AppliedWithChunkAttribute"/>.
+    /// </summary>
+    /// <param name="chunkType">Type of the chunk.</param>
     public AppliedWithChunkAttribute(Type chunkType)
     {
         ChunkType = chunkType;
