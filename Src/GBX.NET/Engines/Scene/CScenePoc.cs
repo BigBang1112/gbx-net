87127abf4c6b0a8a05ef88852833a404eda1604a
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-25 03:07:51 +0200

    Add CSceneMessageHandler, CScenePoc, CSceneSoundSource

---
+++ Src/GBX.NET/Engines/Scene/CScenePoc.cs
@@ -0,0 +1,31 @@
+﻿namespace GBX.NET.Engines.Scene;
+
+/// <remarks>ID: 0x0A009000</remarks>
+[Node(0x0A009000)]
+public abstract class CScenePoc : CSceneObject
+{
+    private bool isActive;
+
+    public bool IsActive { get => isActive; set => isActive = value; }
+
+    protected CScenePoc()
+    {
+        
+    }
+
+    #region 0x000 chunk
+
+    /// <summary>
+    /// CScenePoc 0x000 chunk
+    /// </summary>
+    [Chunk(0x0A009000)]
+    public class Chunk0A009000 : Chunk<CScenePoc>
+    {
+        public override void ReadWrite(CScenePoc n, GameBoxReaderWriter rw)
+        {
+            rw.Boolean(ref n.isActive);
+        }
+    }
+
+    #endregion
+}
---
+++ Src/GBX.NET/Engines/Scene/CSceneSoundSource.cs
@@ -0,0 +1,29 @@
+﻿namespace GBX.NET.Engines.Scene;
+
+/// <remarks>ID: 0x0A00E000</remarks>
+[Node(0x0A00E000)]
+public class CSceneSoundSource : CScenePoc
+{
+    protected CSceneSoundSource()
+    {
+
+    }
+
+    #region 0x000 chunk
+
+    /// <summary>
+    /// CSceneSoundSource 0x000 chunk
+    /// </summary>
+    [Chunk(0x0A00E000)]
+    public class Chunk0A00E000 : Chunk<CSceneSoundSource>
+    {
+        public CHmsSoundSource? U01;
+
+        public override void Read(CSceneSoundSource n, GameBoxReader r, ILogger? logger)
+        {
+            U01 = Parse<CHmsSoundSource>(r, 0x0600D000, progress: null, logger); // Gliding/AfterBurnout/Burnout
+        }
+    }
+
+    #endregion
+}
