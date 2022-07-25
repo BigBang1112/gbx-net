a81a465f2c95f6a8439cd5d8d54fa1e699dda2a4
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-25 03:06:22 +0200

    Add CMwCmd, CMwCmdContainer

---
+++ Src/GBX.NET/Engines/MwFoundations/CMwCmd.cs
@@ -0,0 +1,11 @@
+﻿namespace GBX.NET.Engines.MwFoundations;
+
+/// <remarks>ID: 0x01005000</remarks>
+[Node(0x01005000)]
+public abstract class CMwCmd : CMwNod
+{
+    protected CMwCmd()
+    {
+
+    }
+}
---
+++ Src/GBX.NET/Engines/MwFoundations/CMwCmdContainer.cs
@@ -0,0 +1,11 @@
+﻿namespace GBX.NET.Engines.MwFoundations;
+
+/// <remarks>ID: 0x01093000</remarks>
+[Node(0x01093000)]
+public class CMwCmdContainer : CMwNod
+{
+    protected CMwCmdContainer()
+    {
+        
+    }
+}
