99872c56e38558376d71de55f04a9d211b8e0673
Petr Pivoňka <petrpiv1@gmail.com>
2022-07-25 03:04:38 +0200

    Add CHmsSoundSource

---
+++ Src/GBX.NET/Engines/Hms/CHmsSoundSource.cs
@@ -0,0 +1,52 @@
+﻿namespace GBX.NET.Engines.Hms;
+
+/// <remarks>ID: 0x0600D000</remarks>
+[Node(0x0600D000)]
+public class CHmsSoundSource : CMwNod
+{
+    protected CHmsSoundSource()
+    {
+
+    }
+
+    #region 0x002 chunk
+
+    /// <summary>
+    /// CHmsSoundSource 0x002 chunk
+    /// </summary>
+    [Chunk(0x0600D002)]
+    public class Chunk0600D002 : Chunk<CHmsSoundSource>
+    {
+        public Node? U01;
+        public GameBoxRefTable.File? U01file;
+        public int U02;
+        public bool U03;
+
+        public override void ReadWrite(CHmsSoundSource n, GameBoxReaderWriter rw)
+        {
+            rw.NodeRef(ref U01, ref U01file); // sound
+            rw.Int32(ref U02);
+            rw.Boolean(ref U03);
+        }
+    }
+
+    #endregion
+
+    #region 0x003 chunk
+
+    /// <summary>
+    /// CHmsSoundSource 0x003 chunk
+    /// </summary>
+    [Chunk(0x0600D003)]
+    public class Chunk0600D003 : Chunk<CHmsSoundSource>
+    {
+        public Vec3 U01;
+
+        public override void ReadWrite(CHmsSoundSource n, GameBoxReaderWriter rw)
+        {
+            rw.Vec3(ref U01);
+        }
+    }
+
+    #endregion
+}
