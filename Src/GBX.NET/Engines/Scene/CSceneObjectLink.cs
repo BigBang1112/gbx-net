0771cfced858ca65de1f8f3420393ee3675df4c6
Petr Pivoňka <petrpiv1@gmail.com>
2022-08-03 03:44:07 +0200

    Fix CSceneObjectLink 0x001

--- Src/GBX.NET/Engines/Scene/CSceneObjectLink.cs
+++ Src/GBX.NET/Engines/Scene/CSceneObjectLink.cs
@@ -17,54 +17,34 @@ public class CSceneObjectLink : CMwNod
     [Chunk(0x0A014001)]
     public class Chunk0A014001 : Chunk<CSceneObjectLink>
     {
-        public int U01;
+        public bool U01;
         public int U02;
-        public int U03;
-        public string? U04;
+        public string[]? U03;
         public int U05;
-        public int U06;
-        public int U07;
-        public int U08;
-        public int U09;
-        public int U10;
-        public int U11;
-        public int U12;
-        public int U13;
-        public int U14;
-        public int U15;
-        public int U16;
-        public int U17;
-        public int U18;
+        public Iso4 U06;
+        public bool U18;
         public Node? U19;
 
         public override void ReadWrite(CSceneObjectLink n, GameBoxReaderWriter rw)
         {
             // All guessed, barely anything matches the code
 
-            rw.Int32(ref U01);
+            rw.Boolean(ref U01);
 
-            if (U01 == 0)
+            if (U01)
+            {
+                rw.Int32(ref U02);
+                rw.ArrayId(ref U03);
+                rw.Int32(ref U05);
+            }
+            else
             {
                 rw.NodeRef(ref U19);
             }
 
-            rw.Int32(ref U02);
-            rw.Int32(ref U03);
-            rw.Id(ref U04);
-            rw.Int32(ref U05);
-            rw.Int32(ref U06);
-            rw.Int32(ref U07);
-            rw.Int32(ref U08);
-            rw.Int32(ref U09);
-            rw.Int32(ref U10);
-            rw.Int32(ref U11);
-            rw.Int32(ref U12);
-            rw.Int32(ref U13);
-            rw.Int32(ref U14);
-            rw.Int32(ref U15);
-            rw.Int32(ref U16);
-            rw.Int32(ref U17);
-            rw.Int32(ref U18);
+            rw.Iso4(ref U06);
+
+            rw.Boolean(ref U18);
         }
     }
     
