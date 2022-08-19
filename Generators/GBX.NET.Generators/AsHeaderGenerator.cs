5051cd47e12f848126d8cf2d46e382b0ad4406c9
Petr Pivoňka <petrpiv1@gmail.com>
2022-08-19 23:07:49 +0200

    Add AsHeader source generator

--- Generators/GBX.NET.Generators/AsHeaderGenerator.cs
+++ Generators/GBX.NET.Generators/AsHeaderGenerator.cs
@@ -8,6 +8,8 @@ namespace GBX.NET.Generators;
 [Generator]
 public class AsHeaderGenerator : ISourceGenerator
 {
+    private const string ConstIHeader = "IHeader";
+
     public void Initialize(GeneratorInitializationContext context)
     {
 #if DEBUG
@@ -21,7 +23,29 @@ public class AsHeaderGenerator : ISourceGenerator
     public void Execute(GeneratorExecutionContext context)
     {
         // GBX.NET assembly
-        var enginesNamespace = context.Compilation.GlobalNamespace.NavigateToNamespace("GBX.NET.Engines") ?? throw new Exception("GBX.NET.Engines namespace not found.");
-        var engineTypes = enginesNamespace.GetNamespaceMembers().SelectMany(x => x.GetTypeMembers());
+        var enginesNamespace = context.Compilation
+            .GlobalNamespace
+            .NavigateToNamespace("GBX.NET.Engines") ?? throw new Exception("GBX.NET.Engines namespace not found.");
+
+        var engineTypesWithHeader = enginesNamespace.GetNamespaceMembers()
+            .SelectMany(x => x.GetTypeMembers())
+            .Where(x => x.Interfaces.Any(x => x.Name == ConstIHeader));
+
+        foreach (var type in engineTypesWithHeader)
+        {
+            context.AddSource($"{type.Name}.g.cs", @$"namespace GBX.NET.Engines.{type.ContainingNamespace.Name};
+
+public partial class {type.Name}
+{{
+    /// <summary>
+    /// Gets the node casted to an interface that shows values used in the header.
+    /// </summary>
+    /// <returns>An <see cref=""IHeader""/> of the node.</returns>
+    public {(type.BaseType?.Interfaces.Any(x => x.Name == ConstIHeader) == true ? "new " : "")}IHeader AsHeader()
+    {{
+        return this;
+    }}
+}}");
+        }
     }
 }
