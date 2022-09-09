aec6b234a1eb50e99e67bef9a7b484168a89b076
Petr Pivoňka <petrpiv1@gmail.com>
2022-09-09 06:49:57 +0200

    Make FilePath not nullable (wtf)

--- Src/GBX.NET/FileRef.cs
+++ Src/GBX.NET/FileRef.cs
@@ -21,7 +21,7 @@ public record FileRef
     /// <summary>
     /// File relative to user folder (or Skins folder if <c><see cref="Version"/> &lt;= 1</c>).
     /// </summary>
-    public string? FilePath { get; init; }
+    public string FilePath { get; init; }
 
     /// <summary>
     /// Url of the locator.
@@ -46,7 +46,7 @@ public record FileRef
     /// <param name="checksum">File checksum, should be null if <c><paramref name="version"/> &lt; 3</c>.</param>
     /// <param name="filePath">If <c><paramref name="version"/> &gt; 1</c>, relative to user folder, else relative to Skins folder.</param>
     /// <param name="locatorUrl">Url of the locator.</param>
-    public FileRef(byte version, byte[]? checksum, string? filePath, string? locatorUrl)
+    public FileRef(byte version, byte[]? checksum, string filePath, string? locatorUrl)
     {
         Version = version;
         Checksum = checksum;
@@ -75,7 +75,7 @@ public record FileRef
 
     public static byte[] DefaultChecksum
     {
-        get => new byte[] { 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+        get => new byte[] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
     }
 
     public static readonly FileRef Default = new();
