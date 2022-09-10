814bd8f528b9a97139a1cbb96c7a4905ddef5799
Petr Pivoňka <petrpiv1@gmail.com>
2022-09-10 04:56:53 +0200

    Fix CGameCtnGhost 0x015

--- Src/GBX.NET/Engines/Game/CGameCtnGhost.cs
+++ Src/GBX.NET/Engines/Game/CGameCtnGhost.cs
@@ -918,19 +918,17 @@ public partial class CGameCtnGhost : CGameGhost
 
     #endregion
 
-    #region 0x015 chunk (vehicle)
+    #region 0x015 chunk (ghost nickname)
 
     /// <summary>
-    /// CGameCtnGhost 0x015 chunk
+    /// CGameCtnGhost 0x015 chunk (ghost nickname)
     /// </summary>
-    [Chunk(0x03092015)]
+    [Chunk(0x03092015, "ghost nickname")]
     public class Chunk03092015 : Chunk<CGameCtnGhost>
     {
-        public string? U01;
-
         public override void ReadWrite(CGameCtnGhost n, GameBoxReaderWriter rw)
         {
-            rw.Id(ref U01);
+            rw.Id(ref n.ghostNickname);
         }
     }
 
