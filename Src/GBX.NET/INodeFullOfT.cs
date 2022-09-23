d1bb9c6ff77164d1e58d3373c19cef58bb3c2d6f
Petr Pivoňka <petrpiv1@gmail.com>
2022-09-23 04:53:56 +0200

    Remove ToGbx

--- Src/GBX.NET/INodeFullOfT.cs
+++ Src/GBX.NET/INodeFullOfT.cs
@@ -2,10 +2,5 @@
 
 public interface INodeFull<T> : INodeFull where T : Node
 {
-    /// <summary>
-    /// Wraps this node into <see cref="GameBox{T}"/> object with explicit conversion.
-    /// </summary>
-    /// <typeparam name="T">Type of the node to use on <see cref="GameBox{T}"/>.</typeparam>
-    /// <returns>A <see cref="GameBox{T}"/>.</returns>
-    GameBox<T> ToGbx();
+    
 }
