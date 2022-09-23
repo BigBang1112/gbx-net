876d0e08663bb36fe0ed837bcb77e66b5968b092
Petr Pivoňka <petrpiv1@gmail.com>
2022-09-23 04:53:43 +0200

    Safer declaration

--- Src/GBX.NET/Engines/Script/CScriptTraitsMetadata.ScriptStructTraitBuilder.cs
+++ Src/GBX.NET/Engines/Script/CScriptTraitsMetadata.ScriptStructTraitBuilder.cs
@@ -14,7 +14,7 @@ public partial class CScriptTraitsMetadata
         {
             this.typeBuilder = typeBuilder;
 
-            Members = new();
+            Members = new(typeBuilder.Members);
         }
 
         public ScriptStructTraitBuilder(string structName) : this(new ScriptStructTypeBuilder(structName))
@@ -24,56 +24,56 @@ public partial class CScriptTraitsMetadata
         public ScriptStructTraitBuilder WithBoolean(string name, bool value)
         {
             typeBuilder.WithBoolean(name);
-            Members.Add(name, new ScriptTrait<bool>(new ScriptType(EScriptType.Boolean), value));
+            Members[name] = new ScriptTrait<bool>(new ScriptType(EScriptType.Boolean), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithInteger(string name, int value)
         {
             typeBuilder.WithInteger(name);
-            Members.Add(name, new ScriptTrait<int>(new ScriptType(EScriptType.Integer), value));
+            Members[name] = new ScriptTrait<int>(new ScriptType(EScriptType.Integer), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithReal(string name, float value)
         {
             typeBuilder.WithReal(name);
-            Members.Add(name, new ScriptTrait<float>(new ScriptType(EScriptType.Real), value));
+            Members[name] = new ScriptTrait<float>(new ScriptType(EScriptType.Real), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithText(string name, string value)
         {
             typeBuilder.WithText(name);
-            Members.Add(name, new ScriptTrait<string>(new ScriptType(EScriptType.Text), value));
+            Members[name] = new ScriptTrait<string>(new ScriptType(EScriptType.Text), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithVec2(string name, Vec2 value)
         {
             typeBuilder.WithVec2(name);
-            Members.Add(name, new ScriptTrait<Vec2>(new ScriptType(EScriptType.Vec2), value));
+            Members[name] = new ScriptTrait<Vec2>(new ScriptType(EScriptType.Vec2), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithVec3(string name, Vec3 value)
         {
             typeBuilder.WithVec3(name);
-            Members.Add(name, new ScriptTrait<Vec3>(new ScriptType(EScriptType.Vec3), value));
+            Members[name] = new ScriptTrait<Vec3>(new ScriptType(EScriptType.Vec3), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithInt3(string name, Int3 value)
         {
             typeBuilder.WithInt3(name);
-            Members.Add(name, new ScriptTrait<Int3>(new ScriptType(EScriptType.Int3), value));
+            Members[name] = new ScriptTrait<Int3>(new ScriptType(EScriptType.Int3), value);
             return this;
         }
 
         public ScriptStructTraitBuilder WithInt2(string name, Int2 value)
         {
             typeBuilder.WithInt2(name);
-            Members.Add(name, new ScriptTrait<Int2>(new ScriptType(EScriptType.Int2), value));
+            Members[name] = new ScriptTrait<Int2>(new ScriptType(EScriptType.Int2), value);
             return this;
         }
 
@@ -85,7 +85,7 @@ public partial class CScriptTraitsMetadata
         public ScriptStructTraitBuilder WithStruct(string name, ScriptStructTrait structTrait)
         {
             typeBuilder.WithStruct(name, (ScriptStructType)structTrait.Type);
-            Members.Add(name, structTrait);
+            Members[name] = structTrait;
             return this;
         }
 
