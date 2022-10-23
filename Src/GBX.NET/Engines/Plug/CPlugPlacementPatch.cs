2033187e59a00ea6bfb3302ace117a700f834a63
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-23 20:42:21 +0200

    Add CPlugPlacementPatch

---
+++ Src/GBX.NET/Engines/Plug/CPlugPlacementPatch.cs
@@ -0,0 +1,11 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+/// <remarks>ID: 0x09160000</remarks>
+[Node(0x09160000)]
+public class CPlugPlacementPatch : CPlugRoadChunk
+{
+    internal CPlugPlacementPatch()
+    {
+
+    }
+}
