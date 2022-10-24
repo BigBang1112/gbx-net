e681f2fe67000ed2bf0054f5f937f9c8a51121fd
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-24 17:13:38 +0200

    Add blockinfo format (.bi)

--- Src/GBX.NET/BlockInfo/BlockUnit.cs
+++ Src/GBX.NET/BlockInfo/BlockUnit.cs
@@ -3,10 +3,10 @@
 public class BlockUnit
 {
     public Int3 Coord { get; set; }
-    public int? NorthClip { get; set; }
-    public int? EastClip { get; set; }
-    public int? SouthClip { get; set; }
-    public int? WestClip { get; set; }
-    public int? TopClip { get; set; }
-    public int? BottomClip { get; set; }
+    public string[]? NorthClips { get; set; }
+    public string[]? EastClips { get; set; }
+    public string[]? SouthClips { get; set; }
+    public string[]? WestClips { get; set; }
+    public string[]? TopClips { get; set; }
+    public string[]? BottomClips { get; set; }
 }
