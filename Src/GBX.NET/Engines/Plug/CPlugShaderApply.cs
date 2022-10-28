057a61a5cf947a18c6145dc1aa74656be84001a4
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-28 20:48:55 +0200

    Add more CPlugShaderApply chunks

--- Src/GBX.NET/Engines/Plug/CPlugShaderApply.cs
+++ Src/GBX.NET/Engines/Plug/CPlugShaderApply.cs
@@ -107,4 +107,88 @@ public class CPlugShaderApply : CPlugShaderGeneric
     }
 
     #endregion
+
+    #region 0x00A chunk
+
+    /// <summary>
+    /// CPlugShaderApply 0x00A chunk
+    /// </summary>
+    [Chunk(0x0902600A)]
+    public class Chunk0902600A : Chunk<CPlugShaderApply>
+    {
+        public uint U01;
+        public uint U02;
+        public uint U03;
+
+        public override void ReadWrite(CPlugShaderApply n, GameBoxReaderWriter rw)
+        {
+            rw.UInt32(ref U01); // DoData
+            rw.UInt32(ref U02); // DoData
+            rw.UInt32(ref U03); // DoData
+        }
+    }
+
+    #endregion
+
+    #region 0x00C chunk
+
+    /// <summary>
+    /// CPlugShaderApply 0x00C chunk
+    /// </summary>
+    [Chunk(0x0902600C)]
+    public class Chunk0902600C : Chunk<CPlugShaderApply>
+    {
+        private int listVersion;
+
+        public CPlugBitmapAddress?[]? U01;
+
+        public override void ReadWrite(CPlugShaderApply n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref listVersion);
+            rw.ArrayNode<CPlugBitmapAddress>(ref U01);
+        }
+    }
+
+    #endregion
+
+    #region 0x010 chunk
+
+    /// <summary>
+    /// CPlugShaderApply 0x010 chunk
+    /// </summary>
+    [Chunk(0x09026010)]
+    public class Chunk09026010 : Chunk<CPlugShaderApply>
+    {
+        public byte U01;
+
+        public override void ReadWrite(CPlugShaderApply n, GameBoxReaderWriter rw)
+        {
+            rw.Byte(ref U01); // DoData
+        }
+    }
+
+    #endregion
+
+    #region 0x011 chunk
+
+    /// <summary>
+    /// CPlugShaderApply 0x011 chunk
+    /// </summary>
+    [Chunk(0x09026011)]
+    public class Chunk09026011 : Chunk<CPlugShaderApply>
+    {
+        private int version;
+
+        public Node? U01;
+
+        public int Version { get => version; set => version = value; }
+
+        public override void ReadWrite(CPlugShaderApply n, GameBoxReaderWriter rw)
+        {
+            rw.Int32(ref version);
+            rw.NodeRef(ref U01);
+        }
+    }
+
+    #endregion
 }
