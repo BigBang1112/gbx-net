4a02bb25c2167567e4f7104673d0a239be46593d
Petr Pivoňka <petrpiv1@gmail.com>
2022-10-30 03:19:23 +0100

    Add CPlugLightUserModel

---
+++ Src/GBX.NET/Engines/Plug/CPlugLightUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+/// <remarks>ID: 0x090F9000</remarks>
+[Node(0x090F9000)]
+public class CPlugLightUserModel : CMwNod
+{
+	internal CPlugLightUserModel()
+	{
+
+	}
+}
