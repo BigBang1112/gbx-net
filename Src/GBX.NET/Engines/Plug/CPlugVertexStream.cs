c7f3a93f133a1ad86b5247491a578ef40a352157
Petr Pivoňka <petrpiv1@gmail.com>
2022-11-07 03:14:47 +0100

    Add rough support for CPlugVertexStream

--- Src/GBX.NET/Engines/Plug/CPlugVertexStream.cs
+++ Src/GBX.NET/Engines/Plug/CPlugVertexStream.cs
@@ -19,25 +19,36 @@ public class CPlugVertexStream : CPlug
     [Chunk(0x09056000)]
     public class Chunk09056000 : Chunk<CPlugVertexStream>, IVersionable
     {
-        private int version;
-        
         public int U01;
         public uint U02;
         public Node? U03;
+        public bool U04;
 
-        public int Version { get => version; set => version = value; }
+        public int Version { get; set; }
 
         public override void ReadWrite(CPlugVertexStream n, GameBoxReaderWriter rw)
         {
-            rw.Int32(ref version);
+            rw.VersionInt32(this);
+
             rw.Int32(ref U01);
             rw.UInt32(ref U02); // DoData
             rw.NodeRef(ref U03);
 
-            if (U01 != 0 && U03 is null)
+            if (U01 == 0 || U03 is not null)
             {
-                rw.ArrayArchive<DataDecl>(ref n.dataDecls);
+                return;
             }
+            
+            if (Version == 0)
+            {
+                throw new ChunkVersionNotSupportedException(Version);
+            }
+
+            rw.ArrayArchive<DataDecl>(ref n.dataDecls!, U01);
+
+            rw.Boolean(ref U04);
+
+            rw.UntilFacade(Unknown);
         }
     }
 
@@ -46,11 +57,18 @@ public class CPlugVertexStream : CPlug
     public class DataDecl : IReadableWritable
     {
         public ulong U01;
+        public ushort? U02;
+        public ushort? U03;
 
-        public void ReadWrite(GameBoxReaderWriter rw, int version = 0)
+        public void ReadWrite(GameBoxReaderWriter rw, int version = 0) // version is not really version in this case
         {
             rw.UInt64(ref U01); // DoData
-            // ...
+
+            if (((U01 >> 0x12) & 0x1ff) != 0)
+            {
+                rw.UInt16(ref U02);
+                rw.UInt16(ref U03);
+            }
         }
     }
 }
