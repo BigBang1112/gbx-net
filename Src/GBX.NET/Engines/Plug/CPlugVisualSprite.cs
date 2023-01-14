02220234aa6cb71503d9bca242324e3159bc4033
Petr Pivoňka <petrpiv1@gmail.com>
2023-01-15 00:33:43 +0100

    Enhance CPlugSpriteParam and CPlugVisualSprite

--- Src/GBX.NET/Engines/Plug/CPlugVisualSprite.cs
+++ Src/GBX.NET/Engines/Plug/CPlugVisualSprite.cs
@@ -4,6 +4,12 @@
 [Node(0x09010000)]
 public class CPlugVisualSprite : CPlugVisual3D
 {
+    private CPlugSpriteParam? spriteParam;
+
+    [NodeMember(ExactlyNamed = true)]
+    [AppliedWithChunk<Chunk09010008>]
+    public CPlugSpriteParam? SpriteParam { get => spriteParam; set => spriteParam = value; }
+
     internal CPlugVisualSprite()
     {
 
@@ -48,4 +54,50 @@ public class CPlugVisualSprite : CPlugVisual3D
             rw.Int16(ref U02);
         }
     }
+
+    #region 0x008 chunk
+
+    /// <summary>
+    /// CPlugVisualSprite 0x008 chunk
+    /// </summary>
+    [Chunk(0x09010008)]
+    public class Chunk09010008 : Chunk<CPlugVisualSprite>
+    {
+        public override void Read(CPlugVisualSprite n, GameBoxReader r)
+        {
+            n.spriteParam = Parse<CPlugSpriteParam>(r, 0x090AC000, progress: null, ignoreZeroIdChunk: true); // direct node
+        }
+
+        public override void Write(CPlugVisualSprite n, GameBoxWriter w)
+        {
+            if (n.spriteParam is null)
+            {
+                w.Write(0);
+            }
+            else
+            {
+                n.spriteParam.Write(w);
+            }
+        }
+    }
+
+    #endregion
+
+    #region 0x009 chunk
+
+    /// <summary>
+    /// CPlugVisualSprite 0x009 chunk
+    /// </summary>
+    [Chunk(0x09010009)]
+    public class Chunk09010009 : Chunk<CPlugVisualSprite>
+    {
+        public Rect[]? U01;
+
+        public override void ReadWrite(CPlugVisualSprite n, GameBoxReaderWriter rw)
+        {
+            rw.Array<Rect>(ref U01);
+        }
+    }
+
+    #endregion
 }
