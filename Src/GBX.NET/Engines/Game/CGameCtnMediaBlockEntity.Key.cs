928935d8138a651e82e4b6da6115ac1034f58a72
Petr Pivoňka <petrpiv1@gmail.com>
2023-01-16 19:02:31 +0100

    Fix CGameCtnMediaBlockEntity for TM2020 again

---
+++ Src/GBX.NET/Engines/Game/CGameCtnMediaBlockEntity.Key.cs
@@ -0,0 +1,41 @@
+﻿namespace GBX.NET.Engines.Game;
+
+public partial class CGameCtnMediaBlockEntity
+{
+    public new class Key : CGameCtnMediaBlock.Key
+    {
+        private int u01;
+        private float u02;
+        private int u03;
+        private int u04;
+        private float u05;
+        private float? u06;
+
+        public int U01 { get => u01; set => u01 = value; }
+        public float U02 { get => u02; set => u02 = value; }
+        public int U03 { get => u03; set => u03 = value; }
+        public int U04 { get => u04; set => u04 = value; }
+        public float U05 { get => u05; set => u05 = value; }
+        public float? U06 { get => u06; set => u06 = value; }
+
+        public override void ReadWrite(GameBoxReaderWriter rw, int version = 0)
+        {
+            base.ReadWrite(rw, version);
+
+            rw.Int32(ref u01);
+
+            if (version >= 6)
+            {
+                rw.Single(ref u02);
+                rw.Int32(ref u03);
+                rw.Int32(ref u04);
+                rw.Single(ref u05);
+
+                if (version >= 7)
+                {
+                    rw.Single(ref u06);
+                }
+            }
+        }
+    }
+}
--- Src/GBX.NET/Engines/Game/CGameCtnMediaBlockEntity.cs
+++ Src/GBX.NET/Engines/Game/CGameCtnMediaBlockEntity.cs
@@ -5,7 +5,7 @@
 /// </summary>
 /// <remarks>ID: 0x0329F000</remarks>
 [Node(0x0329F000)]
-public class CGameCtnMediaBlockEntity : CGameCtnMediaBlock, CGameCtnMediaBlock.IHasTwoKeys, CGameCtnMediaBlock.IHasKeys
+public partial class CGameCtnMediaBlockEntity : CGameCtnMediaBlock, CGameCtnMediaBlock.IHasTwoKeys, CGameCtnMediaBlock.IHasKeys
 {
     private CPlugEntRecordData recordData;
     private TimeSingle? start;
@@ -184,9 +184,9 @@ public class CGameCtnMediaBlockEntity : CGameCtnMediaBlock, CGameCtnMediaBlock.I
                             {
                                 rw.Int32(ref U12);
 
-                                if (version >= 9)
+                                if (version >= 10)
                                 {
-                                    rw.UntilFacade(Unknown); // Temporary
+                                    throw new ChunkVersionNotSupportedException(version);
                                 }
                             }
                         }
@@ -198,35 +198,23 @@ public class CGameCtnMediaBlockEntity : CGameCtnMediaBlock, CGameCtnMediaBlock.I
 
     #endregion
 
-    #endregion
+    #region 0x002 chunk
 
-    public new class Key : CGameCtnMediaBlock.Key
+    /// <summary>
+    /// CGameCtnMediaBlockEntity 0x002 chunk
+    /// </summary>
+    [Chunk(0x0329F002)]
+    public class Chunk0329F002 : Chunk<CGameCtnMediaBlockEntity>
     {
-        private int u01;
-        private int u02;
-        private int u03;
-        private int u04;
-        private int u05;
+        public int U01;
 
-        public int U01 { get => u01; set => u01 = value; }
-        public int U02 { get => u02; set => u02 = value; }
-        public int U03 { get => u03; set => u03 = value; }
-        public int U04 { get => u04; set => u04 = value; }
-        public int U05 { get => u05; set => u05 = value; }
-
-        public override void ReadWrite(GameBoxReaderWriter rw, int version = 0)
+        public override void ReadWrite(CGameCtnMediaBlockEntity n, GameBoxReaderWriter rw)
         {
-            base.ReadWrite(rw, version);
-
-            rw.Int32(ref u01);
-
-            if (version >= 6)
-            {
-                rw.Int32(ref u02);
-                rw.Int32(ref u03);
-                rw.Int32(ref u04);
-                rw.Int32(ref u05);
-            }
+            rw.Int32(ref U01);
         }
     }
+
+    #endregion
+
+    #endregion
 }
