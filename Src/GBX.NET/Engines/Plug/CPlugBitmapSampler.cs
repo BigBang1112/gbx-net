cbd9cbdde87d8573f49b9a3c9b3f7465594f0475
Petr Pivoňka <petrpiv1@gmail.com>
2023-02-05 22:44:02 +0100

    Improve CPlugBitmapSampler

--- Src/GBX.NET/Engines/Plug/CPlugBitmapSampler.cs
+++ Src/GBX.NET/Engines/Plug/CPlugBitmapSampler.cs
@@ -7,6 +7,15 @@
 [Node(0x0907E000)]
 public class CPlugBitmapSampler : CPlug
 {
+    private string name = "";
+
+    [NodeMember]
+    [AppliedWithChunk<Chunk0907E002>]
+    [AppliedWithChunk<Chunk0907E005>]
+    [AppliedWithChunk<Chunk0907E006>]
+    [AppliedWithChunk<Chunk0907E008>]
+    public string Name { get => name; set => name = value; }
+
     internal CPlugBitmapSampler()
     {
 
@@ -18,15 +27,15 @@ public class CPlugBitmapSampler : CPlug
     [Chunk(0x0907E002)]
     public class Chunk0907E002 : Chunk<CPlugBitmapSampler>
     {
-        public string? U01;
         public CMwNod? U02;
-        public int? U03;
-        public float? U04;
+        public GameBoxRefTable.File? U02File;
+        public int U03;
+        public float U04;
 
         public override void ReadWrite(CPlugBitmapSampler n, GameBoxReaderWriter rw)
         {
-            rw.Id(ref U01);
-            rw.NodeRef(ref U02);
+            rw.Id(ref n.name!);
+            rw.NodeRef(ref U02, ref U02File);
             rw.Int32(ref U03); // DoData
             rw.Single(ref U04);
         }
@@ -36,40 +45,18 @@ public class CPlugBitmapSampler : CPlug
     /// CPlugBitmapSampler 0x005 chunk
     /// </summary>
     [Chunk(0x0907E005)]
-    public class Chunk0907E005 : Chunk<CPlugBitmapSampler>
+    public class Chunk0907E005 : Chunk0907E002
     {
-        public string? U01;
-        public CMwNod? U02;
-        public int? U03;
-        public float? U04;
-
-        public override void ReadWrite(CPlugBitmapSampler n, GameBoxReaderWriter rw)
-        {
-            rw.Id(ref U01);
-            rw.NodeRef(ref U02);
-            rw.Int32(ref U03);
-            rw.Single(ref U04);
-        }
+        
     }
 
     /// <summary>
     /// CPlugBitmapSampler 0x006 chunk
     /// </summary>
     [Chunk(0x0907E006)]
-    public class Chunk0907E006 : Chunk<CPlugBitmapSampler>
+    public class Chunk0907E006 : Chunk0907E002
     {
-        public string? U01;
-        public CMwNod? U02;
-        public int? U03;
-        public float? U04;
-
-        public override void ReadWrite(CPlugBitmapSampler n, GameBoxReaderWriter rw)
-        {
-            rw.Id(ref U01);
-            rw.NodeRef(ref U02);
-            rw.Int32(ref U03);
-            rw.Single(ref U04);
-        }
+        
     }
 
     #region 0x008 chunk
@@ -78,20 +65,13 @@ public class CPlugBitmapSampler : CPlug
     /// CPlugBitmapSampler 0x008 chunk
     /// </summary>
     [Chunk(0x0907E008)]
-    public class Chunk0907E008 : Chunk<CPlugBitmapSampler>
+    public class Chunk0907E008 : Chunk0907E002
     {
-        public string? U01;
-        public CMwNod? U02;
-        public uint U03;
-        public float U04;
-        public uint? U05;
+        public uint U05;
 
         public override void ReadWrite(CPlugBitmapSampler n, GameBoxReaderWriter rw)
         {
-            rw.Id(ref U01);
-            rw.NodeRef(ref U02);
-            rw.UInt32(ref U03);
-            rw.Single(ref U04);
+            base.ReadWrite(n, rw);
 
             // CPlugBitmapSampler::IsBorderColorUsed
             if (((((byte)U03 & 0x18) != 0x18) && (((byte)U03 & 0x60) != 0x60)) && ((U03 & 0x1800) != 0x1800))
