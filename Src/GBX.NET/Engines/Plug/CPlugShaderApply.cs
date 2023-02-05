70d8f026326c3f8ad15783087126e54f359500f7
Petr Pivoňka <petrpiv1@gmail.com>
2023-02-05 22:43:40 +0100

    Fix CPlugShaderApply 0x001

--- Src/GBX.NET/Engines/Plug/CPlugShaderApply.cs
+++ Src/GBX.NET/Engines/Plug/CPlugShaderApply.cs
@@ -30,14 +30,13 @@ public class CPlugShaderApply : CPlugShaderGeneric
     {
         public int U01;
         public int U02;
-        public int U03;
         public int U04;
 
         public override void ReadWrite(CPlugShaderApply n, GameBoxReaderWriter rw)
         {
             rw.Int32(ref U01);
             rw.Int32(ref U02);
-            rw.Int32(ref U03);
+            rw.ArrayNode<CPlugBitmapAddress>(ref n.bitmapAddresses);
             rw.Int32(ref U04); // DoData
         }
     }
