d8eee6b19728a4b077d5bfc0bfd398da4518b4b8
Petr Pivoňka <petrpiv1@gmail.com>
2023-02-07 22:07:40 +0100

    Add GetNodeFromRefTable(s) support for ExternalNode

--- Src/GBX.NET/Node.cs
+++ Src/GBX.NET/Node.cs
@@ -72,6 +72,26 @@ public abstract class Node
         return type.FullName?.Substring("GBX.NET.Engines".Length + 1).Replace(".", "::") ?? string.Empty;
     }
 
+    protected internal ExternalNode<T>[]? GetNodesFromRefTable<T>(ExternalNode<T>[]? nodesAtTheMoment) where T : Node
+    {
+        if (nodesAtTheMoment is null)
+        {
+            return nodesAtTheMoment;
+        }
+
+        for (var i = 0; i < nodesAtTheMoment.Length; i++)
+        {
+            nodesAtTheMoment[i] = GetNodeFromRefTable(nodesAtTheMoment[i]);
+        }
+
+        return nodesAtTheMoment;
+    }
+
+    protected internal ExternalNode<T> GetNodeFromRefTable<T>(ExternalNode<T> nodeAtTheMoment) where T : Node
+    {
+        return nodeAtTheMoment with { Node = GetNodeFromRefTable(nodeAtTheMoment.Node, nodeAtTheMoment.File) as T };
+    }
+
     protected internal Node? GetNodeFromRefTable(Node? nodeAtTheMoment, GameBoxRefTable.File? nodeFile)
     {
         return GetNodeFromRefTable(gbxForRefTable, nodeAtTheMoment, nodeFile);
