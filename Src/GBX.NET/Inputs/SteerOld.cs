a36cffb2b4e137781cb7d9523e8170af2c7f51e0
Petr Pivoňka <petrpiv1@gmail.com>
2023-02-21 21:12:01 +0100

    Implement IInputSteer on SteerOld

--- Src/GBX.NET/Inputs/SteerOld.cs
+++ Src/GBX.NET/Inputs/SteerOld.cs
@@ -1,6 +1,9 @@
 ﻿namespace GBX.NET.Inputs;
 
-public readonly record struct SteerOld(TimeInt32 Time, float Value) : IInput
+public readonly record struct SteerOld(TimeInt32 Time, float Value) : IInputSteer
 {
-
+    public float GetValue()
+    {
+        return Value;
+    }
 }
