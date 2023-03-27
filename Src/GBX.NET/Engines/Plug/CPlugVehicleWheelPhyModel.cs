977b2fa48e3d2f341389b368eb09ba5ba86c637d
Petr Pivoňka <petrpiv1@gmail.com>
2023-03-28 00:42:08 +0200

    Add CPlugVehicleWheelPhyModel

---
+++ Src/GBX.NET/Engines/Plug/CPlugVehicleWheelPhyModel.cs
@@ -0,0 +1,19 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+public class CPlugVehicleWheelPhyModel : IReadableWritable
+{
+    private string id = "";
+    private bool isDriving;
+    private bool isSteering;
+
+    public string Id { get => id; set => id = value; }
+    public bool IsDriving { get => isDriving; set => isDriving = value; }
+    public bool IsSteering { get => isSteering; set => isSteering = value; }
+
+    public void ReadWrite(GameBoxReaderWriter rw, int version = 0)
+    {
+        rw.Boolean(ref isDriving);
+        rw.Boolean(ref isSteering);
+        rw.Id(ref id!);
+    }
+}
