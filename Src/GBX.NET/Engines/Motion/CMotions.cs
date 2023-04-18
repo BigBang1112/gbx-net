19c575a1662cafe6cf71af613f046a11b3c6aa91
Petr Pivoňka <petrpiv1@gmail.com>
2023-04-19 01:45:32 +0200

    Add CMotions

---
+++ Src/GBX.NET/Engines/Motion/CMotions.cs
@@ -0,0 +1,33 @@
+﻿namespace GBX.NET.Engines.Motion;
+
+/// <remarks>ID: 0x08028000</remarks>
+[Node(0x08028000)]
+public class CMotions : CMotion
+{
+    private CMotion[] motions = Array.Empty<CMotion>();
+
+    [NodeMember(ExactlyNamed = true)]
+    [AppliedWithChunk<Chunk08028001>]
+    public CMotion[] Motions { get => motions; set => motions = value; }
+
+    internal CMotions()
+    {
+
+    }
+
+    #region 0x001 chunk
+
+    /// <summary>
+    /// CMotions 0x001 chunk
+    /// </summary>
+    [Chunk(0x08028001)]
+    public class Chunk08028001 : Chunk<CMotions>
+    {
+        public override void ReadWrite(CMotions n, GameBoxReaderWriter rw)
+        {
+            rw.ArrayNode<CMotion>(ref n.motions!);
+        }
+    }
+
+    #endregion
+}
