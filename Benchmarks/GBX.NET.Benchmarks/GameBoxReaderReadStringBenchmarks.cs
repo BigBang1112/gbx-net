577871e1f62a3da475108c73d0ad3fff65a1c392
Petr Pivoňka <petrpiv1@gmail.com>
2023-11-19 04:40:07 +0100

    Use BenchmarkSwitcher

---
+++ Benchmarks/GBX.NET.Benchmarks/GameBoxReaderReadStringBenchmarks.cs
@@ -0,0 +1,34 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+using System.IO;
+
+namespace GBX.NET.Benchmarks;
+
+[MemoryDiagnoser]
+public class GameBoxReaderReadStringBenchmarks
+{
+    private readonly GameBoxReader reader;
+
+    public GameBoxReaderReadStringBenchmarks()
+    {
+        var stream = new MemoryStream();
+        using var writer = new GameBoxWriter(stream);
+        
+        for (var i = 0; i < 4; i++)
+        {
+            writer.Write("Hello World!");
+        }
+
+        reader = new GameBoxReader(stream);
+    }
+    
+    [Benchmark]
+    public void Read4EqualStrings()
+    {
+        reader.BaseStream.Position = 0; // 1.5 nanosecond operation
+        reader.ReadString();
+        reader.ReadString();
+        reader.ReadString();
+        reader.ReadString();
+    }
+}
---
+++ Benchmarks/GBX.NET.Benchmarks/MapParseBenchmarks.cs
@@ -0,0 +1,23 @@
+﻿using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Order;
+
+namespace GBX.NET.Benchmarks;
+
+[MemoryDiagnoser]
+[Orderer(SummaryOrderPolicy.FastestToSlowest)]
+public class MapParseBenchmarks
+{
+    private readonly MemoryStream stream;
+
+    public MapParseBenchmarks()
+    {
+        stream = new MemoryStream(File.ReadAllBytes(Path.Combine("Maps", "8_Trackmania 2020", "20211001", "Fall 2021 - 16.Map.Gbx")));
+    }
+
+    [Benchmark]
+    public GameBox ParseHeavyMap()
+    {
+        stream.Position = 0;
+        return GameBox.Parse(stream);
+    }
+}
