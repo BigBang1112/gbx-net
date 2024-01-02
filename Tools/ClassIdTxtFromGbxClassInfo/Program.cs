b18c1c1b680542950eb7ac231452302ffcbb3776
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-02 18:12:11 +0100

    Use Engine.txt

--- Tools/ClassIdTxtFromGbxClassInfo/Program.cs
+++ Tools/ClassIdTxtFromGbxClassInfo/Program.cs
@@ -7,7 +7,22 @@ if (args.Length == 0)
     return;
 }
 
-var dict = new Dictionary<uint, string?>();
+var engineDict = new Dictionary<uint, string>();
+
+await foreach (var line in File.ReadLinesAsync("Engine.txt"))
+{
+    var split = line.Split(' ');
+    var engineIdStr = split[0];
+
+    if (!uint.TryParse(engineIdStr, NumberStyles.HexNumber, CultureInfo.InvariantCulture, out var engineId))
+    {
+        continue;
+    }
+
+    engineDict[engineId] = split[1];
+}
+
+var classIdDict = new Dictionary<uint, string?>();
 
 foreach (var fileName in args)
 {
@@ -28,37 +43,15 @@ foreach (var fileName in args)
             name = split[1];
         }
 
-        dict[classId] = name;
+        classIdDict[classId] = name;
     }
 }
 
-foreach (var engineGroup in dict.OrderBy(x => x.Key).GroupBy(x => (x.Key >> 24) & 0xFF))
+foreach (var engineGroup in classIdDict.OrderBy(x => x.Key).GroupBy(x => (x.Key >> 24) & 0xFF))
 {
-    Console.WriteLine($"{engineGroup.Key:X2} {engineGroup.Key switch
-    {
-        0x01 => "MwFoundations",
-        0x03 => "Game",
-        0x04 => "Graphic",
-        0x05 => "Function",
-        0x06 => "Hms",
-        0x07 => "Control",
-        0x09 => "Plug",
-        0x0A => "Scene",
-        0x0B => "System",
-        0x0C => "Vision",
-        0x10 => "Audio",
-        0x11 => "Script",
-        0x12 => "Net",
-        0x13 => "Input",
-        0x14 => "Xml",
-        0x21 => "VirtualSkipper",
-        0x24 => "TrackMania",
-        0x2D => "ShootMania",
-        0x2E => "GameData",
-        0x2F => "Meta",
-        0x30 => "MetaNotPersistent",
-        _ => "",
-    }}");
+    _ = engineDict.TryGetValue(engineGroup.Key, out var engineName);
+
+    Console.WriteLine($"{engineGroup.Key:X2} {engineName}");
 
     foreach (var kvp in engineGroup.OrderBy(x => x.Key))
     {
