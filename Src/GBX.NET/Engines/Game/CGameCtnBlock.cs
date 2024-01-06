a492ae059a9361eac247b12b0754cf13cb275925
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-06 18:35:34 +0100

    Add CGameCtnBlock.Name and Coord

---
+++ Src/GBX.NET/Engines/Game/CGameCtnBlock.cs
@@ -0,0 +1,19 @@
+﻿namespace GBX.NET.Engines.Game;
+
+public partial class CGameCtnBlock
+{
+    /// <summary>
+    /// Name of the block.
+    /// </summary>
+    public string Name
+    {
+        get => blockModel.Id;
+        set => blockModel = blockModel with { Id = value };
+    }
+
+    private Int3 coord;
+    /// <summary>
+    /// Position of the block on the map in block coordination.
+    /// </summary>
+    public Int3 Coord { get => coord; set => coord = value; }
+}
