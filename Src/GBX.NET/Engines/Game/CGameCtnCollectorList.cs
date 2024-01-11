e02bc3067a95750af9e66d1213b9093596f42f4b
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-11 03:29:58 +0100

    Temp implement CGameCtnCollectorList

---
+++ Src/GBX.NET/Engines/Game/CGameCtnCollectorList.cs
@@ -0,0 +1,23 @@
+﻿
+namespace GBX.NET.Engines.Game;
+
+public partial class CGameCtnCollectorList
+{
+    public partial class Chunk0301B000
+    {
+        internal override void Read(CGameCtnCollectorList n, GbxReader r)
+        {
+            var count = r.ReadInt32();
+            for (var i = 0; i < count; i++)
+            {
+                r.ReadIdent();
+                r.ReadInt32();
+            }
+        }
+
+        internal override void Write(CGameCtnCollectorList n, GbxWriter w)
+        {
+            w.Write(0);
+        }
+    }
+}
