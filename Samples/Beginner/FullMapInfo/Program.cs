cc729338a6af2d9193194b3a2bfe5459cc03c66f
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-21 05:34:58 +0100

    Remove double parse from sample

--- Samples/Beginner/FullMapInfo/Program.cs
+++ Samples/Beginner/FullMapInfo/Program.cs
@@ -16,20 +16,9 @@ var map = Gbx.ParseNode<CGameCtnChallenge>(args[0]);
 
 watch.Stop();
 
-using var ms = new MemoryStream();
-using var fs = File.OpenRead(args[0]);
-fs.CopyTo(ms);
-ms.Position = 0;
-
-watch = Stopwatch.StartNew();
-
-var mapAgain = Gbx.ParseNode<CGameCtnChallenge>(ms);
-
-watch.Stop();
-
 Console.WriteLine("Block count per model:");
 
-foreach (var model in mapAgain.Blocks.GroupBy(x => x.BlockModel))
+foreach (var model in map.Blocks.GroupBy(x => x.BlockModel))
 {
     Console.WriteLine($"- {model.Key.Id}: {model.Count()}");
 }
