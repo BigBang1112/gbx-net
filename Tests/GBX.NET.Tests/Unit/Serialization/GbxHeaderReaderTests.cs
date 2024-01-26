e2949d5a96f95169012488d91e994b776011d006
Petr Pivoňka <petrpiv1@gmail.com>
2024-01-26 17:40:30 +0100

    Add ReadUserData tests

--- Tests/GBX.NET.Tests/Unit/Serialization/GbxHeaderReaderTests.cs
+++ Tests/GBX.NET.Tests/Unit/Serialization/GbxHeaderReaderTests.cs
@@ -1,4 +1,6 @@
-﻿using GBX.NET.Exceptions;
+﻿using GBX.NET.Components;
+using GBX.NET.Engines.Game;
+using GBX.NET.Exceptions;
 using GBX.NET.Serialization;
 
 namespace GBX.NET.Tests.Unit.Serialization;
@@ -179,5 +181,300 @@ public class GbxHeaderReaderTests
         Assert.Throws<InvalidDataException>(() => GbxHeaderReader.FillHeaderChunkInfo(headerChunkDescs, reader, userDataInfo));
 
         Assert.Equal(expected: streamPos, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserData_EmptyUserData_ReturnsFalse()
+    {
+        // Arrange
+        using var ms = new MemoryStream(BitConverter.GetBytes(0).ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        // Act
+        var result = parser.ReadUserData(node: null, unknownHeader: null);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ReadUserData_UserDataWithZeroHeaderChunks_ReturnsFalse()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(4)
+            .Concat(BitConverter.GetBytes(0))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        // Act
+        var result = parser.ReadUserData(node: null, unknownHeader: null);
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ReadUserData_UnknownNodeAndNoHeaderObject_Throws()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(16)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043069))
+            .Concat(BitConverter.GetBytes(4))
+            .Concat(BitConverter.GetBytes(6))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        // Act & Assert
+        Assert.Throws<Exception>(() => parser.ReadUserData(node: null, unknownHeader: null));
+    }
+
+    [Fact]
+    public void ReadUserData_UnknownNode_ReadsAndAddsUnknownHeaderChunk()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(16)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043069))
+            .Concat(BitConverter.GetBytes(4))
+            .Concat(BitConverter.GetBytes(6))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var unknownHeader = new GbxHeaderUnknown(GbxHeaderBasic.Default, 0x03043000);
+
+        // Act
+        var result = parser.ReadUserData(node: null, unknownHeader);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(unknownHeader.UserData);
+        Assert.False(unknownHeader.UserData.First().IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: (uint)0x03043069, actual: unknownHeader.UserData.First().Id);
+        Assert.Equal(expected: [6, 0, 0, 0], actual: unknownHeader.UserData.First().Data);
+        Assert.Equal(expected: 20, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserData_KnownNode_ReadsAndAddsUnknownHeaderChunk()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(16)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043069))
+            .Concat(BitConverter.GetBytes(4))
+            .Concat(BitConverter.GetBytes(6))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var node = new CGameCtnChallenge();
+
+        // Act
+        var result = parser.ReadUserData(node, unknownHeader: null);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(node.Chunks);
+        Assert.IsType<HeaderChunk>(node.Chunks.First());
+        Assert.False(((HeaderChunk)node.Chunks.First()).IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: (uint)0x03043069, actual: node.Chunks.First().Id);
+        Assert.Equal(expected: [6, 0, 0, 0], actual: ((HeaderChunk)node.Chunks.First()).Data);
+        Assert.Equal(expected: 20, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserData_KnownNode_CreatesAndReadsKnownHeaderChunk()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(16)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043004))
+            .Concat(BitConverter.GetBytes(4))
+            .Concat(BitConverter.GetBytes(6))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var node = new CGameCtnChallenge();
+
+        // Act
+        var result = parser.ReadUserData(node, unknownHeader: null);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(node.Chunks);
+        Assert.IsType<CGameCtnChallenge.HeaderChunk03043004>(node.Chunks.First());
+        Assert.False(((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: 6, actual: ((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).Version);
+        Assert.Equal(expected: 20, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserData_KnownNode_ChunkLarger_CreatesAndReadsKnownHeaderChunkAndSkipsData()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(20)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043004))
+            .Concat(BitConverter.GetBytes(8))
+            .Concat(BitConverter.GetBytes(6))
+            .Concat(BitConverter.GetBytes(69))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var node = new CGameCtnChallenge();
+
+        // Act
+        var result = parser.ReadUserData(node, unknownHeader: null);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(node.Chunks);
+        Assert.IsType<CGameCtnChallenge.HeaderChunk03043004>(node.Chunks.First());
+        Assert.False(((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: 6, actual: ((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).Version);
+        Assert.Equal(expected: 24, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserDataOfT_EmptyUserData_ReturnsFalse()
+    {
+        // Arrange
+        using var ms = new MemoryStream(BitConverter.GetBytes(0).ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        // Act
+        var result = parser.ReadUserData<CGameCtnChallenge>(new());
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ReadUserDataOfT_UserDataWithZeroHeaderChunks_ReturnsFalse()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(4)
+            .Concat(BitConverter.GetBytes(0))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        // Act
+        var result = parser.ReadUserData<CGameCtnChallenge>(new());
+
+        // Assert
+        Assert.False(result);
+    }
+
+    [Fact]
+    public void ReadUserDataOfT_ReadsAndAddsUnknownHeaderChunk()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(16)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043069))
+            .Concat(BitConverter.GetBytes(4))
+            .Concat(BitConverter.GetBytes(6))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var node = new CGameCtnChallenge();
+
+        // Act
+        var result = parser.ReadUserData(node);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(node.Chunks);
+        Assert.False(((HeaderChunk)node.Chunks.First()).IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: (uint)0x03043069, actual: node.Chunks.First().Id);
+        Assert.Equal(expected: [6, 0, 0, 0], actual: ((HeaderChunk)node.Chunks.First()).Data);
+        Assert.Equal(expected: 20, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserDataOfT_CreatesAndReadsKnownHeaderChunk()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(16)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043004))
+            .Concat(BitConverter.GetBytes(4))
+            .Concat(BitConverter.GetBytes(6))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var node = new CGameCtnChallenge();
+
+        // Act
+        var result = parser.ReadUserData(node);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(node.Chunks);
+        Assert.IsType<CGameCtnChallenge.HeaderChunk03043004>(node.Chunks.First());
+        Assert.False(((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: 6, actual: ((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).Version);
+        Assert.Equal(expected: 20, actual: ms.Position);
+    }
+
+    [Fact]
+    public void ReadUserDataOfT_ChunkLarger_CreatesAndReadsKnownHeaderChunkAndSkipsData()
+    {
+        // Arrange
+        using var ms = new MemoryStream(
+                    BitConverter.GetBytes(20)
+            .Concat(BitConverter.GetBytes(1))
+            .Concat(BitConverter.GetBytes(0x03043004))
+            .Concat(BitConverter.GetBytes(8))
+            .Concat(BitConverter.GetBytes(6))
+            .Concat(BitConverter.GetBytes(69))
+                .ToArray());
+        using var r = new GbxReader(ms);
+
+        var parser = new GbxHeaderReader(r, new());
+
+        var node = new CGameCtnChallenge();
+
+        // Act
+        var result = parser.ReadUserData(node);
+
+        // Assert
+        Assert.True(result);
+        Assert.Single(node.Chunks);
+        Assert.IsType<CGameCtnChallenge.HeaderChunk03043004>(node.Chunks.First());
+        Assert.False(((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).IsHeavy, "Header chunk is heavy but should not be.");
+        Assert.Equal(expected: 6, actual: ((CGameCtnChallenge.HeaderChunk03043004)node.Chunks.First()).Version);
+        Assert.Equal(expected: 24, actual: ms.Position);
     }
 }
