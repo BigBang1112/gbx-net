5cb3db7153304c8684467c45d2e4660753df3eb9
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-01 16:12:33 +0100

    Fix Int4.Zero

--- Src/GBX.NET/Int4.cs
+++ Src/GBX.NET/Int4.cs
@@ -7,5 +7,5 @@ public readonly record struct Int4(int X, int Y, int Z, int W)
         return $"<{X}, {Y}, {Z}, {W}>";
     }
 
-    public static readonly Int2 Zero = new();
+    public static readonly Int4 Zero = new();
 }
