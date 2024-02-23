c6445161ba6014eef1dde15b9b4dfa05e67b457d
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-23 01:05:49 +0100

    Remove BotPath

--- Src/GBX.NET/Engines/Game/CGameCtnChallenge.cs
+++ Src/GBX.NET/Engines/Game/CGameCtnChallenge.cs
@@ -180,28 +180,4 @@ public sealed partial class CGameCtnChallenge :
     {
         string MapUid { get; set; }
     }
-
-    public sealed class BotPath : IReadableWritable
-    {
-        /*private int clan;
-        private IList<Vec3>? path;
-        private bool isFlying;
-        private CGameWaypointSpecialProperty? waypointSpecialProperty;
-        private bool isAutonomous;
-
-        public int Clan { get => clan; set => clan = value; }
-        public IList<Vec3>? Path { get => path; set => path = value; }
-        public bool IsFlying { get => isFlying; set => isFlying = value; }
-        public CGameWaypointSpecialProperty? WaypointSpecialProperty { get => waypointSpecialProperty; set => waypointSpecialProperty = value; }
-        public bool IsAutonomous { get => isAutonomous; set => isAutonomous = value; }*/
-
-        public void ReadWrite(GbxReaderWriter rw, int version = 0)
-        {
-            /*rw.Int32(ref clan);
-            rw.List<Vec3>(ref path, r => r.ReadVec3(), (x, w) => w.Write(x));
-            rw.Boolean(ref isFlying);
-            rw.NodeRef<CGameWaypointSpecialProperty>(ref waypointSpecialProperty);
-            rw.Boolean(ref isAutonomous);*/
-        }
-    }
 }
