0beb9ba60d9c80c13e5d6b71b27a53dacbb83553
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-25 19:47:07 +0100

    Add ParseRepeater

---
+++ Tools/ParseRepeater/Program.cs
@@ -0,0 +1,28 @@
+﻿using GBX.NET;
+using GBX.NET.LZO;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: ParseRepeater <filename>");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey(true);
+}
+
+var fileName = args[0];
+
+Gbx.LZO = new MiniLZO();
+
+while (true)
+{
+    try
+    {
+        var gbx = Gbx.ParseNode(fileName);
+    }
+    catch (Exception ex)
+    {
+        Console.WriteLine("Exception during parse: " + ex);
+    }
+
+    Console.WriteLine();
+    Console.ReadKey(true);
+}
