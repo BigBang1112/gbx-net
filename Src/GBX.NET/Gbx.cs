5eea1c61df079328d107cb661023cc17bde9a919
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-26 10:10:48 +0100

    Add FilePath support

--- Src/GBX.NET/Gbx.cs
+++ Src/GBX.NET/Gbx.cs
@@ -4,32 +4,32 @@ using GBX.NET.Managers;
 
 namespace GBX.NET;
 
-public interface IGbx : ICloneable
+public interface IGbx
 {
-    string? FileName { get; }
+    string? FilePath { get; set; }
     GbxHeader Header { get; }
     GbxRefTable? RefTable { get; }
     IClass? Node { get; }
 
-    void Save(string fileName, ClassIdRemapMode remap = default);
+    void Save(string filePath, ClassIdRemapMode remap = default);
     void Save(Stream stream, ClassIdRemapMode remap = default);
 
 #if NET8_0_OR_GREATER
     static abstract Gbx ParseHeader(Stream stream, GbxReadSettings settings = default);
-    static abstract Gbx ParseHeader(string fileName, GbxReadSettings settings = default);
+    static abstract Gbx ParseHeader(string filePath, GbxReadSettings settings = default);
     static abstract Gbx<T> ParseHeader<T>(Stream stream, GbxReadSettings settings = default) where T : IClass, new();
-    static abstract Gbx<T> ParseHeader<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new();
+    static abstract Gbx<T> ParseHeader<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new();
     static abstract Gbx Parse(Stream stream, GbxReadSettings settings = default);
-    static abstract Gbx Parse(string fileName, GbxReadSettings settings = default);
+    static abstract Gbx Parse(string filePath, GbxReadSettings settings = default);
     static abstract Gbx<T> Parse<T>(Stream stream, GbxReadSettings settings = default) where T : IClass, new();
-    static abstract Gbx<T> Parse<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new();
+    static abstract Gbx<T> Parse<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new();
     static abstract IClass? ParseHeaderNode(Stream stream, GbxReadSettings settings = default);
-    static abstract IClass? ParseHeaderNode(string fileName, GbxReadSettings settings = default);
+    static abstract IClass? ParseHeaderNode(string filePath, GbxReadSettings settings = default);
     static abstract T ParseHeaderNode<T>(Stream stream, GbxReadSettings settings = default) where T : IClass, new();
-    static abstract T ParseHeaderNode<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new();
+    static abstract T ParseHeaderNode<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new();
     static abstract T ParseNode<T>(Stream stream, GbxReadSettings settings = default) where T : IClass, new();
-    static abstract T ParseNode<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new();
-    static abstract IClass? ParseNode(string fileName, GbxReadSettings settings = default);
+    static abstract T ParseNode<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new();
+    static abstract IClass? ParseNode(string filePath, GbxReadSettings settings = default);
     static abstract IClass? ParseNode(Stream stream, GbxReadSettings settings = default);
 #endif
 }
@@ -43,7 +43,7 @@ public class Gbx : IGbx
 {
     public const string Magic = "GBX";
 
-    public string? FileName => throw new NotImplementedException();
+    public string? FilePath { get; set; }
     public GbxHeader Header { get; }
     public GbxRefTable? RefTable { get; private set; }
     public GbxBody? Body { get; private set; }
@@ -71,13 +71,16 @@ public class Gbx : IGbx
         var header = GbxHeader.Parse(reader, settings, out var node);
         var refTable = GbxRefTable.Parse(reader, header, settings);
 
+        var filePath = stream is FileStream fs ? fs.Name : null;
+
         if (node is null) // aka, header is GbxHeaderUnknown
         {
             return new Gbx(header)
             {
                 RefTable = refTable,
                 Body = GbxBody.Parse(reader, header.Basic.CompressionOfBody, settings),
-                ReadSettings = settings
+                ReadSettings = settings,
+                FilePath = filePath
             };
         }
 
@@ -91,13 +94,14 @@ public class Gbx : IGbx
         gbx.IdVersion = reader.IdVersion;
         gbx.PackDescVersion = reader.PackDescVersion;
         gbx.DeprecVersion = reader.DeprecVersion;
+        gbx.FilePath = filePath;
 
         return gbx;
     }
 
-    public static Gbx Parse(string fileName, GbxReadSettings settings = default)
+    public static Gbx Parse(string filePath, GbxReadSettings settings = default)
     {
-        using var fs = File.OpenRead(fileName);
+        using var fs = File.OpenRead(filePath);
         return Parse(fs, settings);
     }
 
@@ -120,13 +124,14 @@ public class Gbx : IGbx
             ReadSettings = settings,
             IdVersion = reader.IdVersion,
             PackDescVersion = reader.PackDescVersion,
-            DeprecVersion = reader.DeprecVersion
+            DeprecVersion = reader.DeprecVersion,
+            FilePath = stream is FileStream fs ? fs.Name : null
         };
     }
 
-    public static Gbx<T> Parse<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new()
+    public static Gbx<T> Parse<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new()
     {
-        using var fs = File.OpenRead(fileName);
+        using var fs = File.OpenRead(filePath);
         return Parse<T>(fs, settings);
     }
 
@@ -138,12 +143,15 @@ public class Gbx : IGbx
         var refTable = GbxRefTable.Parse(reader, header, settings);
         var body = GbxBody.Parse(reader, header.Basic.CompressionOfBody, settings);
 
+        var filePath = stream is FileStream fs ? fs.Name : null;
+        
         if (node is null) // aka, header is GbxHeaderUnknown
         {
             return new Gbx(header)
             {
                 RefTable = refTable,
-                ReadSettings = settings
+                ReadSettings = settings,
+                FilePath = filePath,
             }; 
         }
         
@@ -154,13 +162,14 @@ public class Gbx : IGbx
         gbx.IdVersion = reader.IdVersion;
         gbx.PackDescVersion = reader.PackDescVersion;
         gbx.DeprecVersion = reader.DeprecVersion;
+        gbx.FilePath = filePath;
 
         return gbx;
     }
 
-    public static Gbx ParseHeader(string fileName, GbxReadSettings settings = default)
+    public static Gbx ParseHeader(string filePath, GbxReadSettings settings = default)
     {
-        using var fs = File.OpenRead(fileName);
+        using var fs = File.OpenRead(filePath);
         return ParseHeader(fs, settings);
     }
 
@@ -179,13 +188,14 @@ public class Gbx : IGbx
             ReadSettings = settings,
             IdVersion = reader.IdVersion,
             PackDescVersion = reader.PackDescVersion,
-            DeprecVersion = reader.DeprecVersion
+            DeprecVersion = reader.DeprecVersion,
+            FilePath = stream is FileStream fs ? fs.Name : null
         };
     }
 
-    public static Gbx<T> ParseHeader<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new()
+    public static Gbx<T> ParseHeader<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new()
     {
-        using var fs = File.OpenRead(fileName);
+        using var fs = File.OpenRead(filePath);
         return ParseHeader<T>(fs, settings);
     }
 
@@ -194,9 +204,9 @@ public class Gbx : IGbx
         return ParseHeader(stream, settings).Node;
     }
 
-    public static IClass? ParseHeaderNode(string fileName, GbxReadSettings settings = default)
+    public static IClass? ParseHeaderNode(string filePath, GbxReadSettings settings = default)
     {
-        return ParseHeader(fileName, settings).Node;
+        return ParseHeader(filePath, settings).Node;
     }
 
     public static T ParseNode<T>(Stream stream, GbxReadSettings settings = default) where T : IClass, new()
@@ -204,14 +214,14 @@ public class Gbx : IGbx
         return Parse<T>(stream, settings).Node;
     }
 
-    public static T ParseNode<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new()
+    public static T ParseNode<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new()
     {
-        return Parse<T>(fileName, settings).Node;
+        return Parse<T>(filePath, settings).Node;
     }
 
-    public static IClass? ParseNode(string fileName, GbxReadSettings settings = default)
+    public static IClass? ParseNode(string filePath, GbxReadSettings settings = default)
     {
-        return Parse(fileName, settings).Node;
+        return Parse(filePath, settings).Node;
     }
 
     public static IClass? ParseNode(Stream stream, GbxReadSettings settings = default)
@@ -224,9 +234,9 @@ public class Gbx : IGbx
         return ParseHeader<T>(stream, settings).Node;
     }
 
-    public static T ParseHeaderNode<T>(string fileName, GbxReadSettings settings = default) where T : IClass, new()
+    public static T ParseHeaderNode<T>(string filePath, GbxReadSettings settings = default) where T : IClass, new()
     {
-        return ParseHeader<T>(fileName, settings).Node;
+        return ParseHeader<T>(filePath, settings).Node;
     }
 
     public static TVersion ParseNode<TClass, TVersion>(Stream stream, GbxReadSettings settings = default)
@@ -236,11 +246,11 @@ public class Gbx : IGbx
         return Parse<TClass>(stream, settings).Node;
     }
 
-    public static TVersion ParseNode<TClass, TVersion>(string fileName, GbxReadSettings settings = default)
+    public static TVersion ParseNode<TClass, TVersion>(string filePath, GbxReadSettings settings = default)
         where TClass : IClass, TVersion, new()
         where TVersion : IClassVersion<TClass>
     {
-        return Parse<TClass>(fileName, settings).Node;
+        return Parse<TClass>(filePath, settings).Node;
     }
 
     public static TVersion ParseHeaderNode<TClass, TVersion>(Stream stream, GbxReadSettings settings = default)
@@ -250,11 +260,11 @@ public class Gbx : IGbx
         return ParseHeader<TClass>(stream, settings).Node;
     }
 
-    public static TVersion ParseHeaderNode<TClass, TVersion>(string fileName, GbxReadSettings settings = default)
+    public static TVersion ParseHeaderNode<TClass, TVersion>(string filePath, GbxReadSettings settings = default)
         where TClass : IClass, TVersion, new()
         where TVersion : IClassVersion<TClass>
     {
-        return ParseHeader<TClass>(fileName, settings).Node;
+        return ParseHeader<TClass>(filePath, settings).Node;
     }
 
     public object Clone()
@@ -262,7 +272,7 @@ public class Gbx : IGbx
         throw new NotImplementedException();
     }
 
-    public virtual void Save(string fileName, ClassIdRemapMode remap = 0)
+    public virtual void Save(string filePath, ClassIdRemapMode remap = 0)
     {
         throw new NotImplementedException();
     }
@@ -292,9 +302,9 @@ public class Gbx : IGbx
     /// <exception cref="LzoNotDefinedException"></exception>
     /// <exception cref="VersionNotSupportedException"></exception>
     /// <exception cref="TextFormatNotSupportedException"></exception>
-    public static bool Compress(string inputFileName, Stream output)
+    public static bool Compress(string inputFilePath, Stream output)
     {
-        using var input = File.OpenRead(inputFileName);
+        using var input = File.OpenRead(inputFilePath);
         return Compress(input, output);
     }
 
@@ -304,9 +314,9 @@ public class Gbx : IGbx
     /// <exception cref="LzoNotDefinedException"></exception>
     /// <exception cref="VersionNotSupportedException"></exception>
     /// <exception cref="TextFormatNotSupportedException"></exception>
-    public static bool Compress(Stream input, string outputFileName)
+    public static bool Compress(Stream input, string outputFilePath)
     {
-        using var output = File.Create(outputFileName);
+        using var output = File.Create(outputFilePath);
         return Compress(input, output);
     }
 
@@ -316,10 +326,10 @@ public class Gbx : IGbx
     /// <exception cref="LzoNotDefinedException"></exception>
     /// <exception cref="VersionNotSupportedException"></exception>
     /// <exception cref="TextFormatNotSupportedException"></exception>
-    public static bool Compress(string inputFileName, string outputFileName)
+    public static bool Compress(string inputFilePath, string outputFilePath)
     {
-        using var input = File.OpenRead(inputFileName);
-        using var output = File.Create(outputFileName);
+        using var input = File.OpenRead(inputFilePath);
+        using var output = File.Create(outputFilePath);
         return Compress(input, output);
     }
 
@@ -344,9 +354,9 @@ public class Gbx : IGbx
     /// <exception cref="LzoNotDefinedException"></exception>
     /// <exception cref="VersionNotSupportedException"></exception>
     /// <exception cref="TextFormatNotSupportedException"></exception>
-    public static bool Decompress(string inputFileName, Stream output)
+    public static bool Decompress(string inputFilePath, Stream output)
     {
-        using var input = File.OpenRead(inputFileName);
+        using var input = File.OpenRead(inputFilePath);
         return Decompress(input, output);
     }
 
@@ -356,9 +366,9 @@ public class Gbx : IGbx
     /// <exception cref="LzoNotDefinedException"></exception>
     /// <exception cref="VersionNotSupportedException"></exception>
     /// <exception cref="TextFormatNotSupportedException"></exception>
-    public static bool Decompress(Stream input, string outputFileName)
+    public static bool Decompress(Stream input, string outputFilePath)
     {
-        using var output = File.Create(outputFileName);
+        using var output = File.Create(outputFilePath);
         return Decompress(input, output);
     }
 
@@ -368,10 +378,10 @@ public class Gbx : IGbx
     /// <exception cref="LzoNotDefinedException"></exception>
     /// <exception cref="VersionNotSupportedException"></exception>
     /// <exception cref="TextFormatNotSupportedException"></exception>
-    public static bool Decompress(string inputFileName, string outputFileName)
+    public static bool Decompress(string inputFilePath, string outputFilePath)
     {
-        using var input = File.OpenRead(inputFileName);
-        using var output = File.Create(outputFileName);
+        using var input = File.OpenRead(inputFilePath);
+        using var output = File.Create(outputFilePath);
         return Decompress(input, output);
     }
 }
