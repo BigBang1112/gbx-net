3e57a284a9e954e491270850742d14bed77e36a9
Petr Pivoňka <petrpiv1@gmail.com>
2024-02-28 21:25:39 +0100

    Add GbxToJson

---
+++ Tools/GbxToJson/Program.cs
@@ -0,0 +1,23 @@
+﻿using GBX.NET;
+using GBX.NET.LZO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: GbxToJson <filename>");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey(true);
+}
+
+Gbx.LZO = new MiniLZO();
+
+var gbx = Gbx.Parse(args[0]);
+
+using var stream = Console.OpenStandardOutput();
+
+JsonSerializer.Serialize(stream, gbx, gbx.GetType(), new JsonSerializerOptions
+{
+    WriteIndented = true,
+    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
+});
