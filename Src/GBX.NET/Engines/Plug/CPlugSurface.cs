1b46b9f618797825a0d8890b8901a0463fd4260e
Petr Pivoňka <petrpiv1@gmail.com>
2024-03-07 02:56:40 +0100

    Prepare CPlugSurface

---
+++ Src/GBX.NET/Engines/Plug/CPlugSurface.cs
@@ -0,0 +1,59 @@
+﻿
+namespace GBX.NET.Engines.Plug;
+
+public partial class CPlugSurface
+{
+    // 0 - Sphere
+    // 1 - Ellipsoid
+    // 6 - Box (Primitive)
+    // 7 - Mesh
+    // 8 - VCylinder (Primitive)
+    // 9 - MultiSphere (Primitive)
+    // 10 - ConvexPolyhedron
+    // 11 - Capsule (Primitive)
+    // 12 - Circle (Non3d)
+    // 13 - Compound
+    // 14 - SphereLocated (Primitive)
+    // 15 - CompoundInstance
+    // 16 - Cylinder (Primitive)
+    // 17 - SphericalShell
+
+    internal static ISurf ReadSurf(GbxReader r)
+    {
+        var surfId = r.ReadInt32();
+
+        return surfId switch // ArchiveGmSurf
+        {
+            _ => throw new NotSupportedException("Unknown surf type: " + surfId)
+        };
+    }
+
+    internal static void WriteSurf(ISurf surf, GbxWriter w)
+    {
+        switch (surf)
+        {
+            default:
+                throw new NotSupportedException("Unknown surf type: " + surf.GetType().Name);
+        }
+    }
+
+    public sealed partial class SurfMaterial
+    {
+        public void ReadWrite(GbxReaderWriter rw, int version = 0)
+        {
+            if (rw.Boolean(material is not null))
+            {
+                rw.NodeRef<CPlugMaterial>(ref material);
+            }
+            else
+            {
+                rw.Int16(ref surfaceId);
+            }
+        }
+    }
+
+    public interface ISurf
+    {
+
+    }
+}
---
+++ Src/GBX.NET/Engines/Plug/CPlugSurfaceGeom.cs
@@ -0,0 +1,33 @@
+﻿
+namespace GBX.NET.Engines.Plug;
+
+public partial class CPlugSurfaceGeom
+{
+    private CPlugSurface.ISurf surf;
+    public CPlugSurface.ISurf Surf { get => surf; set => surf = value; }
+
+    public partial class Chunk0900F004
+    {
+        public string? U01;
+        public BoxAligned U02;
+        public ushort U03;
+
+        public override void ReadWrite(CPlugSurfaceGeom n, GbxReaderWriter rw)
+        {
+            rw.Id(ref U01);
+            rw.BoxAligned(ref U02);
+
+            if (rw.Reader is not null)
+            {
+                n.Surf = CPlugSurface.ReadSurf(rw.Reader);
+            }
+            
+            if (rw.Writer is not null)
+            {
+                CPlugSurface.WriteSurf(n.Surf, rw.Writer);
+            }
+
+            rw.UInt16(ref U03);
+        }
+    }
+}
