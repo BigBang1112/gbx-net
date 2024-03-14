054263e77d26a6b8b18084dd1a8989604d97001e
Petr Pivoňka <petrpiv1@gmail.com>
2024-03-14 21:27:02 +0100

    Add Directory and File methods

--- Src/GBX.NET/Components/GbxRefTableDirectory.cs
+++ Src/GBX.NET/Components/GbxRefTableDirectory.cs
@@ -7,6 +7,16 @@ public sealed class GbxRefTableDirectory : IDirectory
     public IList<IDirectory> Children { get; } = new List<IDirectory>();
     public IList<IFile> Files { get; } = new List<IFile>();
 
+    public IDirectory? Directory(string name)
+    {
+        return Children.FirstOrDefault(x => x.Name == name);
+    }
+
+    public IFile? File(string name)
+    {
+        return Files.FirstOrDefault(x => x.Name == name);
+    }
+
     public override string ToString()
     {
         return string.IsNullOrEmpty(Name) ? "(root)" : Name;
--- Src/GBX.NET/IDirectory.cs
+++ Src/GBX.NET/IDirectory.cs
@@ -2,7 +2,11 @@
 
 public interface IDirectory
 {
+    string Name { get; set; }
     IDirectory? Parent { get; set; }
     IList<IDirectory> Children { get; }
     IList<IFile> Files { get; }
+
+    IDirectory? Directory(string name);
+    IFile? File(string name);
 }
