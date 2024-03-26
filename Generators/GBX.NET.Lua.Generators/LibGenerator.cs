c93a048906771b5b521f0b04dd21415fc88e015b
Petr Pivoňka <petrpiv1@gmail.com>
2024-03-26 14:54:29 +0100

    Add GBX.NET.Lua.Generators

---
+++ Generators/GBX.NET.Lua.Generators/LibGenerator.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.CodeAnalysis;
+using System.Diagnostics;
+using System.Text;
+
+namespace GBX.NET.Generators;
+
+[Generator(LanguageNames.CSharp)]
+public class LibGenerator : IIncrementalGenerator
+{
+    private const bool Debug = false;
+
+    public void Initialize(IncrementalGeneratorInitializationContext context)
+    {
+        if (Debug && !Debugger.IsAttached)
+        {
+            Debugger.Launch();
+        }
+    }
+}
