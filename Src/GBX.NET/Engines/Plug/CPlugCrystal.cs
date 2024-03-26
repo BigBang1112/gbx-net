abf9a94c77574e551917e17d5d3e91c153a35587
Petr Pivoňka <petrpiv1@gmail.com>
2024-03-26 01:53:11 +0100

    Add more layers

--- Src/GBX.NET/Engines/Plug/CPlugCrystal.cs
+++ Src/GBX.NET/Engines/Plug/CPlugCrystal.cs
@@ -35,7 +35,76 @@ public partial class CPlugCrystal
     public abstract partial class Layer;
 
     [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public abstract partial class ModifierLayer
+    {
+        public override void Read(GbxReader r, int version = 0)
+        {
+            base.Read(r, version);
+            modifierVersion = r.ReadInt32();
+            mask = r.ReadArrayReadable<PartInLayer>();
+        }
+
+        public override void Write(GbxWriter w, int version = 0)
+        {
+            base.Write(w, version);
+            w.Write(modifierVersion);
+            w.WriteArrayWritable<PartInLayer>(mask);
+        }
+    }
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class PartInLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
     public partial class GeometryLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class SubdivideSmoothLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class TranslationLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class RotationLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class ScaleLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class MirrorLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class MoveToGroundLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class ExtrudeLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class SubdivideLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class ChaosLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class SmoothLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class BorderTransitionLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class DeformationLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class CubesLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class TriggerLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class SpawnPositionLayer;
+
+    [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
+    public partial class VoxelSpace;
 
     [ArchiveGenerationOptions(StructureKind = StructureKind.SeparateReadAndWrite)]
     public partial class Crystal;
