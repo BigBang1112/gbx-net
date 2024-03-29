04f96415edfcc0a9695fbe220d7bb318e659ea15
Petr Pivoňka <petrpiv1@gmail.com>
2024-03-29 06:05:37 +0100

    Add GbxDiff

---
+++ Tools/GbxDiff/Program.cs
@@ -0,0 +1,40 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+using GBX.NET.LZO;
+using KellermanSoftware.CompareNetObjects;
+
+if (args.Length < 2)
+{
+    Console.WriteLine("Usage: GbxDiff <file1> <file2>");
+    return;
+}
+
+Console.WriteLine("Expected: " + Path.GetFileName(args[0]));
+Console.WriteLine("Actual: " + Path.GetFileName(args[1]));
+Console.WriteLine();
+
+Gbx.LZO = new MiniLZO();
+
+var gbx1 = Gbx.Parse(args[0]);
+var gbx2 = Gbx.Parse(args[1]);
+
+var compareLogic = new CompareLogic(new ComparisonConfig() { MaxDifferences = 100 });
+
+var result = compareLogic.Compare(gbx1, gbx2);
+
+if (!result.AreEqual)
+{
+    Console.WriteLine("Files are not equal:");
+    Console.WriteLine();
+
+    foreach (var difference in result.Differences)
+    {
+        Console.WriteLine(difference.ToString());
+    }
+}
+else
+{
+    Console.WriteLine("Files are equal.");
+}
+
+Console.ReadKey();
