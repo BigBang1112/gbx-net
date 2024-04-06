5e11590fdd25454267b0e94e34127355fc810858
Petr Pivoňka <petrpiv1@gmail.com>
2024-04-06 20:07:14 +0200

    Add CPlugPrefab (progress broken - bug in CPlugSurface)

---
+++ Src/GBX.NET/Engines/Plug/CPlugPrefab.cs
@@ -0,0 +1,37 @@
+﻿
+namespace GBX.NET.Engines.Plug;
+
+public partial class CPlugPrefab : IVersionable
+{
+    private DateTime fileWriteTime;
+    private string url = "";
+    private int u01;
+    private int u02;
+    private EntRef[] ents = [];
+
+    public int Version { get; set; }
+
+    public DateTime FileWriteTime { get => fileWriteTime; set => fileWriteTime = value; }
+    public string Url { get => url; set => url = value; }
+    public int U01 { get => u01; set => u01 = value; }
+    public int U02 { get => u02; set => u02 = value; }
+    public EntRef[] Ents { get => ents; set => ents = value; }
+
+#if NET8_0_OR_GREATER
+    static void IClass.Read<T>(T node, GbxReaderWriter rw)
+    {
+        node.ReadWrite(rw);
+    }
+#endif
+
+    public override void ReadWrite(GbxReaderWriter rw)
+    {
+        rw.VersionInt32(this);
+        rw.FileTime(ref fileWriteTime);
+        rw.String(ref url);
+        rw.Int32(ref u01);
+        var entsLength = rw.Int32(ents.Length);
+        rw.Int32(ref u02);
+        rw.ArrayReadableWritable<EntRef>(ref ents, entsLength);
+    }
+}
