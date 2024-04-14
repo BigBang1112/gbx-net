2ecc30849bc0f150a744f77a21b09f1921ee27a5
Petr Pivoňka <petrpiv1@gmail.com>
2024-04-14 05:27:02 +0200

    Add ZLib

---
+++ Src/GBX.NET.ZLib/README.md
@@ -0,0 +1,1 @@
+# GBX.NET.Zlib
---
+++ Src/GBX.NET.ZLib/ZLib.cs
@@ -0,0 +1,39 @@
+﻿using GBX.NET.Extensions;
+
+namespace GBX.NET.ZLib;
+
+public sealed class ZLib : IZLib
+{
+    private readonly ZLibDotNet.ZLib zlib = new();
+
+    public byte[] Compress(ReadOnlySpan<byte> input)
+    {
+        var sourceLen = zlib.CompressBound((uint)input.Length);
+
+        var output = new byte[sourceLen];
+
+        ZLibDotNet.ZStream zStream = new()
+        {
+            Input = input,
+            Output = output
+        };
+
+        _ = zlib.DeflateInit(ref zStream, ZLibDotNet.ZLib.Z_DEFAULT_COMPRESSION);
+        _ = zlib.Deflate(ref zStream, ZLibDotNet.ZLib.Z_FULL_FLUSH);
+        _ = zlib.DeflateEnd(ref zStream);
+
+        return output;
+    }
+
+    public void Decompress(ReadOnlySpan<byte> input, Span<byte> output)
+    {
+        var zStream = new ZLibDotNet.ZStream
+        {
+            Input = input,
+            Output = output
+        };
+
+        _ = zlib.InflateInit(ref zStream);
+        _ = zlib.Inflate(ref zStream, ZLibDotNet.ZLib.Z_SYNC_FLUSH);
+    }
+}
