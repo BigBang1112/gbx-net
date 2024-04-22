036220758a5afe551899bccc429189fe28781e63
Petr Pivoňka <petrpiv1@gmail.com>
2024-04-22 17:01:32 +0200

    Add CPlugTree.GetAllChildren

---
+++ Src/GBX.NET/Engines/Plug/CPlugTree.cs
@@ -0,0 +1,25 @@
+﻿namespace GBX.NET.Engines.Plug;
+
+public partial class CPlugTree
+{
+    public IEnumerable<CPlugTree> GetAllChildren()
+    {
+        return GetAllChildren(this);
+        
+        static IEnumerable<CPlugTree> GetAllChildren(CPlugTree tree)
+        {
+            if (tree.Children is null)
+            {
+                yield break;
+            }
+
+            foreach (var child in tree.Children)
+            {
+                foreach (var descendant in GetAllChildren(child))
+                {
+                    yield return descendant;
+                }
+            }
+        }
+    }
+}
