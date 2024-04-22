aa65c2fe10a1862d0c2107f0b56e8c1a920135c1
Petr Pivoňka <petrpiv1@gmail.com>
2024-04-22 17:50:18 +0200

    Fix tree name being saved as actual collection name

--- Src/GBX.NET/Engines/Plug/CPlugTree.cs
+++ Src/GBX.NET/Engines/Plug/CPlugTree.cs
@@ -2,6 +2,8 @@
 
 public partial class CPlugTree
 {
+    public string Name { get; set; } = "";
+
     public IEnumerable<CPlugTree> GetAllChildren()
     {
         return GetAllChildren(this);
@@ -20,6 +22,29 @@ public partial class CPlugTree
                     yield return descendant;
                 }
             }
+        }
+    }
+
+    public partial class Chunk0904F00D
+    {
+        public override void Read(CPlugTree n, GbxReader r)
+        {
+            n.Name = r.ReadIdAsString();
+            U01 = r.ReadIdAsString();
+        }
+
+        public override void Write(CPlugTree n, GbxWriter w)
+        {
+            if (int.TryParse(n.Name, out var numericName) && (numericName & 0xC0000000) == 0)
+            {
+                w.Write(new Id(numericName));
+            }
+            else
+            {
+                w.WriteIdAsString(n.Name);
+            }
+
+            w.WriteIdAsString(U01);
         }
     }
 }
