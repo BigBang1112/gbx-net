d447a67fc8636175a01e5746515841421252beba
Petr Pivoňka <petrpiv1@gmail.com>
2024-04-28 01:30:01 +0200

    Make some attributes public

--- Src/GBX.NET/Attributes/ChunkGameVersionAttribute.cs
+++ Src/GBX.NET/Attributes/ChunkGameVersionAttribute.cs
@@ -1,7 +1,7 @@
 ﻿namespace GBX.NET.Attributes;
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Struct)]
-internal sealed class ChunkGameVersionAttribute(GameVersion game, params int[] version) : Attribute
+public sealed class ChunkGameVersionAttribute(GameVersion game, params int[] version) : Attribute
 {
     public GameVersion Game { get; } = game;
     public int[] Version { get; } = version;
