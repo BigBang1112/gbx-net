c3f609b1e43d4ed70a45aa8c0474277c3c08ff7c
Petr Pivoňka <petrpiv1@gmail.com>
2024-05-01 22:44:06 +0200

    Add TMF CGamePlayerProfile

---
+++ Src/GBX.NET/Engines/Game/CGamePlayerProfile.cs
@@ -0,0 +1,38 @@
+﻿
+namespace GBX.NET.Engines.Game;
+
+public partial class CGamePlayerProfile
+{
+    public partial class Chunk0308C068
+    {
+        public Dictionary<string, int>? U01;
+
+        public override void Read(CGamePlayerProfile n, GbxReader r)
+        {
+            var count = r.ReadInt32();
+            U01 = new(count);
+
+            for (var i = 0; i < count; i++)
+            {
+                U01.Add(r.ReadIdAsString(), r.ReadInt32());
+            }
+        }
+
+        public override void Write(CGamePlayerProfile n, GbxWriter w)
+        {
+            if (U01 is null)
+            {
+                w.Write(0);
+                return;
+            }
+
+            w.Write(U01.Count);
+
+            foreach (var pair in U01)
+            {
+                w.WriteIdAsString(pair.Key);
+                w.Write(pair.Value);
+            }
+        }
+    }
+}
