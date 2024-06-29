8465b547adbf42bf14c300065aa50cbd6838cb32
Petr Pivoňka <petrpiv1@gmail.com>
2024-06-29 21:28:51 +0200

    Add GBX.NET.NewtonsoftJson

---
+++ Src/GBX.NET.NewtonsoftJson/JsonExtensions.cs
@@ -0,0 +1,61 @@
+﻿using GBX.NET.Engines.MwFoundations;
+using GBX.NET.NewtonsoftJson.Converters;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace GBX.NET.NewtonsoftJson;
+
+public static class JsonExtensions
+{
+    private static readonly JsonSerializerSettings settings = new()
+    {
+        TypeNameHandling = TypeNameHandling.Auto,
+        NullValueHandling = NullValueHandling.Ignore
+    };
+
+    static JsonExtensions()
+    {
+        settings.Converters.Add(new TimeInt32JsonConverter());
+        settings.Converters.Add(new NullableTimeInt32JsonConverter());
+        settings.Converters.Add(new StringEnumConverter());
+    }
+
+    public static string ToJson(this Gbx gbx, bool indented = true)
+    {
+        return JsonConvert.SerializeObject(gbx, indented ? Formatting.Indented : Formatting.None, settings);
+    }
+
+    public static string ToJson(this CMwNod node, bool indented = true)
+    {
+        return JsonConvert.SerializeObject(node, indented ? Formatting.Indented : Formatting.None, settings);
+    }
+
+    public static void ToJson(this Gbx gbx, TextWriter writer, bool indented = true)
+    {
+        CreateSerializer(indented).Serialize(writer, gbx);
+    }
+
+    public static void ToJson(this CMwNod node, TextWriter writer, bool indented = true)
+    {
+        CreateSerializer(indented).Serialize(writer, node);
+    }
+
+    public static void ToJson(this Gbx gbx, Stream stream, bool indented = true)
+    {
+        using var writer = new StreamWriter(stream);
+        CreateSerializer(indented).Serialize(writer, gbx);
+    }
+
+    public static void ToJson(this CMwNod node, Stream stream, bool indented = true)
+    {
+        using var writer = new StreamWriter(stream);
+        CreateSerializer(indented).Serialize(writer, node);
+    }
+
+    private static JsonSerializer CreateSerializer(bool indented)
+    {
+        var serializer = JsonSerializer.Create(settings);
+        serializer.Formatting = indented ? Formatting.Indented : Formatting.None;
+        return serializer;
+    }
+}
--- Tools/GbxToJson/Program.cs
+++ Tools/GbxToJson/Program.cs
@@ -1,8 +1,6 @@
 ﻿using GBX.NET;
-using GBX.NET.Json.Converters;
+using GBX.NET.NewtonsoftJson;
 using GBX.NET.LZO;
-using System.Text.Json;
-using System.Text.Json.Serialization;
 
 if (args.Length == 0)
 {
@@ -15,18 +13,4 @@ Gbx.LZO = new MiniLZO();
 
 var gbx = Gbx.Parse(args[0]);
 
-using var stream = Console.OpenStandardOutput();
-
-var options = new JsonSerializerOptions
-{
-    WriteIndented = true,
-    DefaultIgnoreCondition = JsonIgnoreCondition.WhenWritingNull
-};
-options.Converters.Add(new JsonStringTimeInt32Converter());
-options.Converters.Add(new JsonNumberTimeSingleConverter());
-options.Converters.Add(new JsonInt3Converter());
-options.Converters.Add(new JsonStringEnumConverter());
-options.Converters.Add(new JsonChunkConverter());
-options.Converters.Add(new JsonClassConverter());
-
-JsonSerializer.Serialize(stream, gbx, gbx.GetType(), options);
+gbx.ToJson(Console.Out);
