7b490c5f7b3623a64ee90452e06f74d6140d62e6
Petr Pivoňka <petrpiv1@gmail.com>
2024-07-05 19:27:50 +0200

    Add NativeSharpLzo to GBX.NET.LZO

---
+++ Src/GBX.NET.LZO/Lzo.cs
@@ -0,0 +1,21 @@
+﻿using GBX.NET.Extensions;
+
+namespace GBX.NET.LZO;
+
+public sealed class Lzo : ILzo
+{
+    public byte[] Compress(byte[] data)
+    {
+        return SharpLzo.Lzo.Compress(SharpLzo.CompressionMode.Lzo1x_999, data);
+    }
+
+    public void Decompress(in Span<byte> input, byte[] output)
+    {
+        var result = SharpLzo.Lzo.TryDecompress(input, input.Length, output, out var _);
+        
+        if (result != SharpLzo.LzoResult.OK)
+        {
+            throw new SharpLzo.LzoException(result);
+        }
+    }
+}
