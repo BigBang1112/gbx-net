0a4b900ccd07b7b5f5fd400da45cca0953f7b17c
Petr Pivoňka <petrpiv1@gmail.com>
2024-07-25 22:24:37 +0200

    Add MethodGameVersionAttribute

---
+++ Src/GBX.NET/Attributes/MethodGameVersionAttribute.cs
@@ -0,0 +1,9 @@
+﻿namespace GBX.NET.Attributes;
+
+[AttributeUsage(AttributeTargets.Method)]
+public sealed class MethodGameVersionAttribute(GameVersion game, params int[] version) : Attribute
+{
+    public GameVersion Game { get; } = game;
+    public int[] Version { get; } = version;
+}
+
