b3180ba7e2c87e81cb7a51d240f1d36e7d4dab8d
Petr Pivoňka <petrpiv1@gmail.com>
2024-07-26 15:18:01 +0200

    Add Mux (without read/write)

---
+++ Src/GBX.NET.Crypto/Mux.cs
@@ -0,0 +1,15 @@
+﻿namespace GBX.NET.Crypto;
+
+public sealed class Mux
+{
+    public byte Version { get; }
+    public int KeySalt { get; }
+    public byte[] Data { get; }
+
+    public Mux(byte version, int keySalt, byte[] data)
+    {
+        Version = version;
+        KeySalt = keySalt;
+        Data = data;
+    }
+}
