25e6fd7afcf13318187732b31d7c2f150247a4db
Petr Pivoňka <petrpiv1@gmail.com>
2024-11-05 20:53:56 +0100

    Support writable CGameCtnBlockInfoVariant

--- Src/GBX.NET/Engines/Game/CGameCtnBlockInfoVariant.cs
+++ Src/GBX.NET/Engines/Game/CGameCtnBlockInfoVariant.cs
@@ -33,5 +33,27 @@ public partial class CGameCtnBlockInfoVariant
                 }
             }
         }
+
+        public override void Write(CGameCtnBlockInfoVariant n, GbxWriter w)
+        {
+            w.Write(Version);
+
+            w.Write(n.Mobils?.Length ?? 0);
+            for (var i = 0; i < n.Mobils?.Length; i++)
+            {
+                w.WriteArrayNodeRef(n.Mobils[i]);
+            }
+
+            if (Version >= 2)
+            {
+                w.Write(U01);
+                w.Write(U02);
+
+                if (Version >= 3)
+                {
+                    w.Write(U03);
+                }
+            }
+        }
     }
 }
