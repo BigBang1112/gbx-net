0cb2f8987ca6c517e4b1a4095b1e39e79376e4aa
Petr Pivoňka <petrpiv1@gmail.com>
2025-01-01 01:18:22 +0100

    Remove updater

--- Tools/GbxExplorerOld/Client/Program.cs
+++ Tools/GbxExplorerOld/Client/Program.cs
@@ -6,7 +6,6 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using System.Globalization;
 using Blazored.LocalStorage;
 using Microsoft.AspNetCore.Components.Authorization;
-using Toolbelt.Blazor.Extensions.DependencyInjection;
 
 CultureInfo.DefaultThreadCurrentCulture = CultureInfo.InvariantCulture;
 
@@ -20,7 +19,6 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddAuthorizationCore();
 builder.Services.AddBlazoredLocalStorage();
-builder.Services.AddPWAUpdater();
 
 builder.Services.AddScoped<AuthenticationStateProvider, DiscordAuthenticationStateProvider>();
 
