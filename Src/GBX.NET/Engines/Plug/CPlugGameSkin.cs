838db08cde4874513bb228362cc3061778a7f7ee
Petr Pivoňka <petrpiv1@gmail.com>
2025-05-23 22:03:03 +0200

    Fix CPlugGameSkin for TM1.0

--- Src/GBX.NET/Engines/Plug/CPlugGameSkin.cs
+++ Src/GBX.NET/Engines/Plug/CPlugGameSkin.cs
@@ -2,13 +2,90 @@
 
 public partial class CPlugGameSkin
 {
+    [AppliedWithChunk<Chunk090F4001>]
+    public Fid[]? CustomizableFids2 { get; set; }
+
+    public partial class Chunk090F4000
+    {
+        public override void Read(CPlugGameSkin n, GbxReader r)
+        {
+            n.CustomizableFids = new Fid[r.ReadInt32()];
+            r.ReadInt32(); // CustomizableNames count thats not used in loop
+            for (var i = 0; i < n.CustomizableFids.Length; i++)
+            {
+                n.CustomizableFids[i] = new Fid
+                {
+                    Node = r.ReadNodeRef<CMwNod>(out var nodeFile),
+                    Name = r.ReadString(),
+                    NodeFile = nodeFile,
+                };
+            }
+        }
+
+        public override void Write(CPlugGameSkin n, GbxWriter w)
+        {
+            var count = n.CustomizableFids?.Length ?? 0;
+            w.Write(count);
+            w.Write(count); // CustomizableNames count that is not used in loop
+
+            if (n.CustomizableFids is null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                var fid = n.CustomizableFids[i];
+                w.WriteNodeRef(fid.Node, fid.NodeFile);
+                w.Write(fid.Name);
+            }
+        }
+    }
+
+    public partial class Chunk090F4001
+    {
+        public override void Read(CPlugGameSkin n, GbxReader r)
+        {
+            n.CustomizableFids2 = new Fid[r.ReadInt32()];
+            r.ReadInt32(); // CustomizableNames count thats not used in loop
+            for (var i = 0; i < n.CustomizableFids2.Length; i++)
+            {
+                n.CustomizableFids2[i] = new Fid
+                {
+                    Node = r.ReadNodeRef<CMwNod>(out var nodeFile),
+                    Name = r.ReadString(),
+                    NodeFile = nodeFile,
+                };
+            }
+        }
+
+        public override void Write(CPlugGameSkin n, GbxWriter w)
+        {
+            var count = n.CustomizableFids2?.Length ?? 0;
+            w.Write(count);
+            w.Write(count); // CustomizableNames count that is not used in loop
+
+            if (n.CustomizableFids2 is null)
+            {
+                return;
+            }
+
+            for (var i = 0; i < count; i++)
+            {
+                var fid = n.CustomizableFids2[i];
+                w.WriteNodeRef(fid.Node, fid.NodeFile);
+                w.Write(fid.Name);
+            }
+        }
+    }
+
     public partial class Fid : IReadableWritable
     {
         private uint classId;
         private string name = "";
         private CMwNod? node;
         private string directory = "";
-        private bool u01;
+        private bool mipMaps;
 
         public uint ClassId { get => classId; set => classId = value; }
         public string Name { get => name; set => name = value; }
@@ -19,7 +96,7 @@ public partial class CPlugGameSkin
         public CMwNod? GetNode(GbxReadSettings settings = default, bool exceptions = false) => nodeFile?.GetNode(ref node, settings, exceptions) ?? node;
 
         public string Directory { get => directory; set => directory = value; }
-        public bool U01 { get => u01; set => u01 = value; }
+        public bool MipMaps { get => mipMaps; set => mipMaps = value; }
 
         public void ReadWrite(GbxReaderWriter rw, int version = 0)
         {
@@ -40,13 +117,21 @@ public partial class CPlugGameSkin
 
             if (version >= 2)
             {
-                rw.Boolean(ref u01);
+                rw.Boolean(ref mipMaps);
             }
         }
 
         public override string ToString()
         {
-            return $"[{ClassId:X8}] {Name} ({Node})";
+            return $"[0x{ClassId:X8}] {Name} ({Node?.ToString() ?? Directory})";
+        }
+    }
+
+    public partial class HeaderFid
+    {
+        public override string ToString()
+        {
+            return $"[0x{ClassId:X8}] {Name} ({Directory})";
         }
     }
 }
