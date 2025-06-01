eb0a53cd517539472e28f40ffd81093ff3048c8e
Petr Pivoňka <petrpiv1@gmail.com>
2025-06-01 02:47:08 +0200

    Add template system

---
+++ Templates/MyGbxConsoleApp/Program.cs
@@ -0,0 +1,47 @@
+﻿using GBX.NET;
+using GBX.NET.Engines.Game;
+#if (EnableLzo)
+using GBX.NET.LZO;
+#endif
+#if (EnableZlib)
+using GBX.NET.ZLib;
+#endif
+
+#if (EnableLzo)
+Gbx.LZO = new Lzo();
+#endif
+#if (EnableZlib)
+Gbx.ZLib = new ZLib();
+#endif
+
+if (args.Length == 0)
+{
+    Console.WriteLine("Usage: MyGbxConsoleApp <path to file>");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey(true);
+    return;
+}
+
+#if (SaveGbx)
+var gbx = Gbx.Parse(args[0]);
+var node = gbx.Node;
+#else
+var node = Gbx.ParseNode(args[0]);
+#endif
+
+if (node is not CGameCtnChallenge map)
+{
+    Console.WriteLine("The Gbx file is not a map.");
+    Console.WriteLine("Press any key to continue...");
+    Console.ReadKey(true);
+    return;
+}
+
+Console.WriteLine($"MapName: {map.MapName}");
+
+#if (SaveGbx)
+map.MapName += $"$z - {DateTime.Now:yyyy-MM-dd HH:mm:ss}";
+
+var newFileName = $"{gbx.GetFileNameWithoutExtension()}-{DateTime.Now:yyyyMMddTHHmmss}{GbxPath.GetExtension(gbx.FilePath)}";
+gbx.Save(newFileName);
+#endif
