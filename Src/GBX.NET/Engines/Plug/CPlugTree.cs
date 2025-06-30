077cc0a87080fea49f83100284ce9a90ba72a963
Petr Pivoňka <petrpiv1@gmail.com>
2025-06-30 02:40:57 +0200

    Fix MultiplyAddIso4

--- Src/GBX.NET/Engines/Plug/CPlugTree.cs
+++ Src/GBX.NET/Engines/Plug/CPlugTree.cs
@@ -111,9 +111,9 @@ public partial class CPlugTree
                 a.ZX * b.XY + a.ZY * b.YY + a.ZZ * b.ZY,
                 a.ZX * b.XZ + a.ZY * b.YZ + a.ZZ * b.ZZ,
 
-                a.TX + b.TX,
-                a.TY + b.TY,
-                a.TZ + b.TZ
+                a.XX * b.TX + a.XY * b.TY + a.XZ * b.TZ + a.TX,
+                a.YX * b.TX + a.YY * b.TY + a.YZ * b.TZ + a.TY,
+                a.ZX * b.TX + a.ZY * b.TY + a.ZZ * b.TZ + a.TZ
             );
         }
 
