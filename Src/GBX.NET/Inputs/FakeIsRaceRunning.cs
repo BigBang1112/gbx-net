89863f296e86ca8533c3af3f5cf5ffb4cfa6ffea
Petr Pivoňka <petrpiv1@gmail.com>
2025-12-17 00:08:13 +0100

    Add a way that's not breaking

--- Src/GBX.NET/Inputs/FakeIsRaceRunning.cs
+++ Src/GBX.NET/Inputs/FakeIsRaceRunning.cs
@@ -1,6 +1,8 @@
 ﻿namespace GBX.NET.Inputs;
 
-public readonly partial record struct FakeIsRaceRunning(TimeInt32 Time, uint Data) : IInput
+public readonly partial record struct FakeIsRaceRunning(TimeInt32 Time, uint Data) : IInputState
 {
+    public bool Pressed => Data != 0;
 
+    public FakeIsRaceRunning(TimeInt32 Time, bool Pressed) : this(Time, Pressed ? 1u : 0u) { }
 }
