0aa65b03b154d5a12420c93b4f19db313df93857
Petr Pivoňka <petrpiv1@gmail.com>
2026-01-28 00:48:46 +0100

    Add CGameCtnChallengeParameters 0x00F

---
+++ Src/GBX.NET/Engines/Game/CGameCtnChallengeParameters.cs
@@ -0,0 +1,38 @@
+﻿namespace GBX.NET.Engines.Game;
+
+public partial class CGameCtnChallengeParameters
+{
+    public partial class Chunk0305B00F
+    {
+        public int U01;
+
+        public override void Read(CGameCtnChallengeParameters n, GbxReader r)
+        {
+            U01 = r.ReadInt32(); // always 0
+            var size = r.ReadInt32();
+
+            using var _ = new Encapsulation(r);
+            
+            n.raceValidateGhost = r.ReadNodeRef<CGameCtnGhost>();
+
+            if (n.raceValidateGhost is not null)
+            {
+                return;
+            }
+        }
+
+        public override void Write(CGameCtnChallengeParameters n, GbxWriter w)
+        {
+            w.Write(U01);
+
+            using var ms = new MemoryStream();
+            using var wBuffer = new GbxWriter(ms);
+            using var _ = new Encapsulation(wBuffer);
+
+            wBuffer.WriteNodeRef(n.raceValidateGhost);
+
+            w.Write((int)ms.Length);
+            ms.WriteTo(w.BaseStream);
+        }
+    }
+}
